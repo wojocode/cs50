3989b8f3740af0f3592a550919949c30fb3b6760
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-10-10 20:37:12 +0000

    Mon, Oct 10, 2022, 8:37 PM +00:00

---
+++ points.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void){
+
+    int points = get_int("How many points did you lose?\n");
+    if (points < 2){
+        printf("You lose )
+        }
+    }
+}
