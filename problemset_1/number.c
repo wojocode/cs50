42ade7ed86dcc850cab15f13c36aa803d48d22df
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-10-10 20:59:56 +0000

    Mon, Oct 10, 2022, 8:59 PM +00:00

--- problemset_1/number.c
+++ problemset_1/number.c
@@ -1,12 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
-
+int main(void)
+{
     int x = get_int("x: ");
 
     if(x / 2 == 0){
         printf("number is even");
     }
     else {
-        printf("number is odd);
+        printf("number is odd");
     }
 }
