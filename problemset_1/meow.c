57e88851fc2cdd5b118995303a4ce17784870ee8
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-10-15 10:45:17 +0000

    Sat, Oct 15, 2022, 10:45 AM +00:00

--- problemset_1/meow.c
+++ problemset_1/meow.c
@@ -1,5 +1,18 @@
-
-
+#include <stdio.h>
+ //deklaracja, ze program będzie korzystał z tej funkcji//
+void meow(int n);
+ //głwny program, wykorzystujący z funkcją meow//
+int main(void){
+    meow(3);
+}
+//działanie funkcji meow//
+void meow(int n)
+{
+    for(int i = 0; i < 3; i++)
+    {
+        printf("meow\n");
+    }
+}
 
 
 
