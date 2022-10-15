45f1e8aaae3b213c021fae13d5c831c2ae30babc
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-10-15 11:57:54 +0000

    Sat, Oct 15, 2022, 11:57 AM +00:00

--- problemset_1/discout_own_funkction.c
+++ problemset_1/discout_own_funkction.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <cs50.h>
-float discount(float price);
+float discount(float price, int procent);
 int main(void)
 {
     float regular = get_float("Regular Price: ");
-    float sale = discount(regular);
+    float procent_off = get_int("Procent 0ff: ");
+    float sale = discount(regular,procent_off);
     printf("Sale Price: %.2f\n", sale);
 }
-float discount(float price)
+float discount(float price, int procent)
 {
-    return price * 0.85;
+    return price * (100 - procent / 100);
 }
