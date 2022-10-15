be4bdd342715789b052d4003912636063012bdde
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-10-15 21:01:40 +0000

    Sat, Oct 15, 2022, 9:01 PM +00:00

--- problemset_1/bmi.c
+++ problemset_1/bmi.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <cs50.h>
 
-float bmi(float h, int w);
+float bmi(int w , float h)
 
 int main(void)
 {
     float height = get_float("How tall are you ? ");
     int weight = get_int("How much do you weight ? ");
     float BMI = bmi(height,weight);
-    printf("Your BMI is: %f\n", BMI);
+    printf("Your BMI is: %.2f\n", BMI);
 }
 
-float bmi(float h, int w)
+float bmi(int w , float h)
 {
     return w / ((h/100)*(h/100));
 }
