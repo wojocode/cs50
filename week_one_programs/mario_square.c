ca69e5840b2f8c80c71e79876f05bf2bc6f65ba1
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-10-16 10:08:31 +0000

    Sun, Oct 16, 2022, 10:08 AM +00:00

---
+++ week_one_programs/mario_square.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+int n;
+do
+{
+    n = get_int("Size: ");
+}
+while (n < 1);
+
+for (int col = 0; col < n; col++)
+{
+    for (int row = 0; row < n; row++)
+    {
+        printf("@");
+    }
+    printf("\n");
+}
+}
