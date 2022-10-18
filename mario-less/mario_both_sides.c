bb45c69a63bf14726b5f3e1405e2708b23419c88
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-10-18 16:06:59 +0000

    Tue, Oct 18, 2022, 4:06 PM +00:00

---
+++ mario-less/mario_both_sides.c
@@ -0,0 +1,44 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while(n < 1 || n > 8);
+
+    for (int row = 0; row < n; row++)
+    {
+        // pętla do robienia odstępow
+        for(int space = 1; space < (n - row); space++)
+        {
+            printf(" ");
+        }
+        for(int i = 0; i <=row; i++)
+        {
+            printf("#");
+        }
+        // koniec 1 pętli z wyrwnaniem do prawej
+        //odstęp
+        printf("  ");
+        //początek 2 pętli
+        for(int row = 0; row < n; row++)
+    {
+
+        for(int i = 0; i <= row; i++)
+        {
+            printf("#");
+        }
+
+    }
+
+        // koniec 2 pętli z wyrwnaniem do lewej
+
+
+         printf("\n");
+    }
+
+}
