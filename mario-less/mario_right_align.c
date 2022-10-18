6c221a75459e63d817ad4fd428ac6d1a70d70b33
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-10-18 15:52:24 +0000

    Tue, Oct 18, 2022, 3:52 PM +00:00

--- mario-less/mario_right_align.c
+++ mario-less/mario_right_align.c
@@ -12,7 +12,7 @@ int main(void)
 
     for (int row = 0; row < n; row++)
     {
-        // pętla do robienia odstępow 
+        // pętla do robienia odstępow
         for(int space = 1; space < (8 - row); space++)
         {
             printf(" ");
