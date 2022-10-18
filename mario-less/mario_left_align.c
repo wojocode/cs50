8c573ad762ee87f89899259285712bb3ad6c7916
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-10-18 14:56:41 +0000

    Tue, Oct 18, 2022, 2:56 PM +00:00

--- mario-less/mario_left_align.c
+++ mario-less/mario_left_align.c
@@ -18,7 +18,6 @@ int main(void)
         {
             printf("#");
         }
-        
         printf("\n");
     }
 }
---
+++ mario-less/mario_right_align.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while(n < 1 || n > 8);
+
+    
+}
