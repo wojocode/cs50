beafa83a632a2e84c1ff02857d5ebf052c13271a
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-10-24 20:38:56 +0000

    Mon, Oct 24, 2022, 8:38 PM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -47,23 +47,23 @@ int get_cents(void)
 int calculate_quarters(int cents)
 {
     // TODO
-    return 0;
+    return cents / 25;
 }
 
 int calculate_dimes(int cents)
 {
     // TODO
-    return 0;
+    return cents / 10;
 }
 
 int calculate_nickels(int cents)
 {
     // TODO
-    return 0;
+    return cents / 5;
 }
 
 int calculate_pennies(int cents)
 {
     // TODO
-    return 0;
+    return cents / 1;
 }
