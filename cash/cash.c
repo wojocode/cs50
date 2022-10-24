7613cead029a62bb0d9d40c4222058da6ee0229b
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-10-24 20:27:20 +0000

    Mon, Oct 24, 2022, 8:27 PM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -10,9 +10,9 @@ int calculate_pennies(int cents);
 int main(void)
 {
     // Ask how many cents the customer is owed
-    int cents = get_cents();
+        int cents=get_cents();
     do {
-        get_cents();
+         int cents=get_cents();
     }
     while(cents < 0);
 
