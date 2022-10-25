127d39a2e9d29a27041b9f5fc93900c5fc51f2ab
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-10-25 21:42:21 +0000

    Tue, Oct 25, 2022, 9:42 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,37 +1,35 @@
+@@ -1,26 +1,26 @@
 #include <cs50.h>
 #include <stdio.h>
-
 int main(void)
 {
 //prototypes
-    int calculate_checksum_first(long card_number,long i);
-
+    int calculate_checksum(long card_number,long i);
 // prompt for input
     long card_number;
-    int sum;
-
     do
     {
      card_number = get_long("enter card number: ");
     }
     while (card_number < 0);
-
-    for (long i = 10; i < card_number; i = i * 10)
+    for (long i = 10; i < card_number; i = i * 100)
     {
-        sum = calculate_checksum_first(card_number,i);
+        int sum = calculate_checksum(card_number,i);
 
     }
-    printf("%i",sum);
-}
+
+        printf("%i",sum);
+    }
+
+
 
 //checksum function
-    int calculate_checksum_first(long card_number,long i)
+    int calculate_checksum(long card_number,long i)
 {
-        int multiplied_digit = card_number % (i * 10) / i;
+        int multiplied_digit = (card_number % (10 * i)) / i;
         multiplied_digit = multiplied_digit * 2;
         return multiplied_digit;
 }
-
 
 
 
