dc386e08bb5063070b6a0bbf2c5d6a1d795e7e86
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-10-25 18:18:32 +0000

    Tue, Oct 25, 2022, 6:18 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -18,7 +18,7 @@ int main(void)
     for (long i = 10; i < card_number; i = i * 100)
     {
         sum = calculate_checksum_first(card_number,i);
-        sum=sum+sum;
+        sum= sum + sum;
     }
     printf("%i", sum);
 
