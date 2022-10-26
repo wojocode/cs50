8423e55e00596f20f08a71f4e1f7d9753db93489
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-10-26 22:14:06 +0000

    Wed, Oct 26, 2022, 10:14 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -35,9 +35,9 @@ int main(void)
             //int rest = sum % 10;
             //all = number + rest;
             //}
-           while(all == sum){
+           while(){
                 all = all + sum;
-               
+
            }
 
     }
