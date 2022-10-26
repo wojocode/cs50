47cc0d6a3020937941bef6d2a0f541179e3a55a1
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-10-26 20:45:09 +0000

    Wed, Oct 26, 2022, 8:45 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -44,9 +44,9 @@ int main(void)
 //checksum function
     int calculate_checksum(long card_number,long i)
 {
-
+        for (long i = 10; i < card_number; i = i * 100){
         return (card_number % (10 * i)) / i;
-
+        }
         //multiplied_digit = multiplied_digit * 2;
 
 }
