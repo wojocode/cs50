d6b3e8255ce79bb5106258d165149f4bece6694d
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-10-30 11:35:19 +0000

    Sun, Oct 30, 2022, 11:35 AM +00:00

--- week_two_programms/scores.c
+++ week_two_programms/scores.c
@@ -7,9 +7,8 @@ int n = get_int("How many scores: ");
 
 int scores[n];
 
-for( i = 0; i < n; i++){
-
-
+for(int i = 0; i < n; i++){
+    scores[i] = get_int("score: ");
 }
-
+printf("Your average is: %i\n", (scores))
 }
