f03f018c479bdade1f397d8c059c0486dd02804b
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-01 20:15:37 +0000

    Tue, Nov 1, 2022, 8:15 PM +00:00

---
+++ week_two_programms/main_cl_arg.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+
+    if(argc == 2)
+    {
+        printf("Hello, %s\n", argv[1]);
+    }
+    else{
+        printf("Hello, World\n");
+    }
+}
