64c56c98da94a11f1803c208b9849284e91cceb1
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-04 18:30:17 +0000

    Fri, Nov 4, 2022, 6:30 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -8,7 +8,14 @@ int main(void)
 {
     string text = get_string("type in text: ");
     printf("%s\n" ,text);
+     int letters = count_letters(text);
+     printf("letters: %i\n", letters);
 
 
 
+}
+
+int count_letters(string text){
+
+    return isalpha(text);
 }
