a2b7e23ada43d7011c7e2dafc0426a6d85e1f456
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-05 09:43:52 +0000

    Sat, Nov 5, 2022, 9:43 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -12,9 +12,9 @@ int main(void)
 {
     do{
     string text = get_string("type in text: ");
-    int n =strlen(text);
+    int n = strlen(text);
     }
-    while(text[n] == 46 || text[n] == 63 || text[n] == 33)
+    while (text[n] == 46 || text[n] == 63 || text[n] == 33)
 
     float letters = count_letters(text);
     float words = count_words(text);
