5450f10b12f3682ef2e914b03ae5f5141ec0ad6b
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-05 10:45:42 +0000

    Sat, Nov 5, 2022, 10:45 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -36,13 +36,13 @@ int main(void){
     if (grade < 1){
 
         printf("Before Grade 1\n");
-   }
-   else if (grade >= 16){
+    }
+    else if (grade >= 16){
         printf("Grade 16+\n");
-   }
-   else {
+    }
+    else {
     printf("Grade %i\n", grade);
-   }
+    }
 
 }
 //letters counter
