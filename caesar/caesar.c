6d4326a89925124c66ae6132e08def03c515fd46
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-06 16:17:45 +0000

    Sun, Nov 6, 2022, 4:17 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,36 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
-int main(int argc, string argv[]) 
-{
+bool only_digits(string argv);
 
+int main(int argc, string argv[]){
+
+    while(argc == 2){
+    if (only_digits(argv[1])){
+        return 0;
+    }
+    else {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    }
+    printf("Usage: ./caesar key\n");
+    return 1;
+}
+
+
+// checking wheather argv contains digits
+bool only_digits(string argv){
+
+    for (int i = 0, n = strlen(argv); i < n; i++){
+
+        if (isdigit(argv[i])){
+        }
+        else{
+            return 0;
+        }
+    }
+    return 1;
 }
