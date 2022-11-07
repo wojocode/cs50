5b5e881d6cbad392b5534b5c9bb5caf4a5698e43
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-07 22:37:15 +0000

    Mon, Nov 7, 2022, 10:37 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,4 +1,3 @@
-#include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
@@ -6,46 +5,34 @@
 
 bool only_digits(string argv);
 
-int main(int argc, string argv[])
-{
-
-    //while (argc == 2)
-   // {
-        if (only_digits(argv[1]))
-        {
-            return 0;
-        }
-        else
-        {
-            printf("Usage: ./caesar key\n");
-            return 1;
-        }
-   // }
-    //if (argc != 2)
-    //{
-    //    printf("Usage: ./caesar key\n");
-    //    return 1;
-   // }
-
-    // convert string command line argv to int
+int main(int argc, string argv[]){
+    while(argc == 2){
+    if (only_digits(argv[1])){
+        return 0;
+    }
+    else {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    }
+    printf("Usage: ./caesar key\n");
+    return 1;
+    //convert string command line argv to int
     int key = atoi(argv[1]);
-    // ask for string
-    string plaintext = get_string("plaintext: ");
-    printf("%i\n", key);
+    //ask for string
+    string plaintext = get_string("plaintext: \n");
+
+
+
 }
 
+
 // checking wheather argv contains digits
-bool only_digits(string argv)
-{
-
-    for (int i = 0, n = strlen(argv); i < n; i++)
-    {
-
-        if (isdigit(argv[i]))
-        {
+bool only_digits(string argv){
+    for (int i = 0, n = strlen(argv); i < n; i++){
+        if (isdigit(argv[i])){
         }
-        else
-        {
+        else{
             return 0;
         }
     }
