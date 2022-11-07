0bb08042c4d35f3090637eb267912b10efe45237
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-07 22:33:20 +0000

    Mon, Nov 7, 2022, 10:33 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -20,11 +20,13 @@ int main(int argc, string argv[]){
             return 1;
         }
 
-     while (argc != 2){
-        printf("Usage: ./caesar key\n");
+     if (argc == 2){
+         return 0;
+        }
+    else{
+         printf("Usage: ./caesar key\n");
         return 1;
         }
-        
 
 
     // convert string command line argv to int
