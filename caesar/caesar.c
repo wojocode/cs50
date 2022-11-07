d19ee124df3b7aee6b5cc3d882f3fe45efed5ff6
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-07 22:26:15 +0000

    Mon, Nov 7, 2022, 10:26 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,38 +6,46 @@
 
 bool only_digits(string argv);
 
-int main(int argc, string argv[]){
+int main(int argc, string argv[])
+{
 
-    while(argc == 2){
-        if (only_digits(argv[1])){
+    while (argc == 2)
+    {
+        if (only_digits(argv[1]))
+        {
             return 0;
         }
-        else {
+        else
+        {
             printf("Usage: ./caesar key\n");
             return 1;
         }
-        }
-        if (argc != 2){
+    }
+    if (argc != 2)
+    {
         printf("Usage: ./caesar key\n");
         return 1;
-        }
+    }
 
-    //convert string command line argv to int
+    // convert string command line argv to int
     int key = atoi(argv[1]);
-    //ask for string
+    // ask for string
     string plaintext = get_string("plaintext: ");
-    printf("%i\n",key);
+    
 }
 
-
 // checking wheather argv contains digits
-bool only_digits(string argv){
+bool only_digits(string argv)
+{
 
-    for (int i = 0, n = strlen(argv); i < n; i++){
+    for (int i = 0, n = strlen(argv); i < n; i++)
+    {
 
-        if (isdigit(argv[i])){
+        if (isdigit(argv[i]))
+        {
         }
-        else{
+        else
+        {
             return 0;
         }
     }
