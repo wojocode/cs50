7a4e6acafe8bb4156095f546afd70383f2570bf2
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-08 21:38:29 +0000

    Tue, Nov 8, 2022, 9:38 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -7,11 +7,11 @@
 bool only_digits(string argv);
 
 int main(int argc, string argv[]){
-    while (argc == 1 || argc >=2){
+    while (argc == 1 || argc > 2){
         printf("Usage: ./caesar key\n");
         return 1;
     }
-    
+
 
 
 
