f6ec575bdf0966d4f7eca65579d4f5c7a7b3220a
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-09 10:19:36 +0000

    Wed, Nov 9, 2022, 10:19 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -29,14 +29,9 @@ int main(int argc, string argv[]){
     char cipher = rotate(plaintext[0], key);
     printf("%i\n" ,cipher);
 
-    printf("%i\n/", plaintext[0]);
-
     //for (int i = 0, n = strlen(plaintext); i < n; i++){
         //char c = plaintext[i];
         //tutaj wywołujemy funcje rotate
-
-
-
     //}
 
 
