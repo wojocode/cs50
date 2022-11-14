01cdfbe81b4a17500fb46beecb05f2021273afaa
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-14 10:06:35 +0000

    Mon, Nov 14, 2022, 10:06 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -6,19 +6,25 @@
 bool only_characters(string key);
 int main(int argc, string argv[])
 {
-    //only 2 command line arg
+//only 2 command line arg
     while (argc == 1 || argc > 2){
         printf("./substitution key\n");
         return 1;
     }
-    // checking cipher lenght
+// checking cipher lenght
     while (strlen(argv[1]) != 26){
         printf("Key must contain 26 characters.\n");
         return 1;
     }
+// checking if key is alphabetical
 
 
 }
+
+
+
+
+// checking if key is alphabetical
 bool only_characters(string key){
     for (int i = 0 , n = strlen(key); i <= n; i++){
         if(isalpha(key[i])){
