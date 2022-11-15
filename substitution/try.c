0fd13340bd58544d90ccd43d28262dcbe2da0a56
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-15 10:48:03 +0000

    Tue, Nov 15, 2022, 10:48 AM +00:00

---
+++ substitution/try.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+bool unique_chars(string key);
+int main(void){
+    string text = get_string("string: \n");
+    if (unique_chars(text)){
+        printf("cool\n");
+    }
+    else {
+        return 1;
+    }
+}
+
+
+bool unique_chars(string key){
+    int i = 0;
+    for (int fixed = 0; fixed < strlen(key); fixed++){
+
+        for(i = 0; i < strlen(key);i++){
+            if (key[fixed] != key[i+1]){
+            }
+            else {
+                return 0;
+            }
+        }
+
+    }
+    return 1;
+}
