cf2a294ab787b187bcdce8bbf88c035cb8884ec8
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-15 11:44:06 +0000

    Tue, Nov 15, 2022, 11:44 AM +00:00

--- substitution/try.c
+++ substitution/try.c
@@ -18,7 +18,7 @@ int main(void){
 
 bool unique_chars(string key){
 
-    for (int fixed = 0 , i = 0; fixed < strlen(key); fixed++, i--){
+    for (int fixed = 0 , i = 0; fixed < strlen(key); fixed++){
 
         for (int d = strlen(key) ; (i + 1) < d;i++){
             if (key[fixed] != key[i+1]){
