e911ace5e18cbc4886774a8bae5b6521871f4889
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-15 09:45:50 +0000

    Tue, Nov 15, 2022, 9:45 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -34,7 +34,7 @@ int main(int argc, string argv[])
 bool only_characters(string key){
     for (int i = 0 , n = strlen(key); i < n; i++){
 // only unique characters check
-        if(isalpha(key[i]) && ((key[i] != key[i+1]) && (key[i] != key[i-1]))){
+        if(isalpha(key[i]) && ){
         }
         else{
             return 0;
