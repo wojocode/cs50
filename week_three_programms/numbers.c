4ca0e52b173f3ea2f540609b69938ee0c5fb14f6
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-18 20:44:58 +0000

    Fri, Nov 18, 2022, 8:44 PM +00:00

--- week_three_programms/numbers.c
+++ week_three_programms/numbers.c
@@ -6,9 +6,9 @@ int main(void){
     int numbers[] = {4,6,8,2,7,5,0};
 
     for (int i = 0; i < 7; i++){
-        if ( numbers[i] == 8){
-            return 0;
+        if ( numbers[i] == 0){
+            printf("Found\n");
         }
     }
-    return 1;
+    printf("not found\n");
 }
