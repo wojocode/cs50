59953b122b0d576b07de6e08c1709ecc9bfdc67f
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-18 22:08:01 +0000

    Fri, Nov 18, 2022, 10:08 PM +00:00

--- week_three_programms/phonebook.c
+++ week_three_programms/phonebook.c
@@ -6,5 +6,13 @@ int main(void){
 
     string names[] = {"Carter","David"};
     string numbers[] = {"+48-660-511-677", "+48-660-511-678"};
-    
+
+    for (int i = 0; i < 2; i++){
+        if (strcmp(names[i], "David") == 0){
+            printf("David %s\n", numbers[i]);
+            return 0;
+        }
+    }
+    printf("Contact not found\n");
+    return 1;
 }
