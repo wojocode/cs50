9e6afd8a2f9d565216d005b776b991f3870aa50e
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-19 11:29:20 +0000

    Sat, Nov 19, 2022, 11:29 AM +00:00

---
+++ week_three_programms/phonebook_new_data_type.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+typedef struct{
+    string name;
+    string number;
+}
+person;
+
+int main(void){
+    
+    person names[] = {"Carter","David"};
+    string numbers[] = {"+48-660-511-677", "+48-660-511-678"};
+
+    for (int i = 0; i < 2; i++){
+        if (strcmp(names[i], "David") == 0){
+            printf("David %s\n", numbers[i]);
+            return 0;
+        }
+    }
+    printf("Contact not found\n");
+    return 1;
+}
