634f494a6bbdba2b4cf6b2fdec30fb365908ba55
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-20 11:07:52 +0000

    Sun, Nov 20, 2022, 11:07 AM +00:00

--- week_three_programms/recursion.c
+++ week_three_programms/recursion.c
@@ -9,9 +9,12 @@ int main(void){
     draw(height);
 }
 
-void draw(int n){
+void draw(int n)
+{
+    if (n <= 0){
+        return;
+    }
     draw(n-1);
-
     for (int i = 0; i < n; i++){
         printf("#");
     }
