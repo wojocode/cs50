eb72c82cd9afb849a62bf2626f1a2dc3567ef594
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-22 16:20:42 +0000

    Tue, Nov 22, 2022, 4:20 PM +00:00

---
+++ week_three_programms/impower_recurence.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cs50.h>
+
+long potega(int w, int p);
+
+int main(void)
+{
+
+
+
+}
+long potega(int w, int p)
+{
+    if (p == 0)
+    {
+        return 1;
+    }
+    return potega*
+}
