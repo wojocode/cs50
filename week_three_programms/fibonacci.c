79fabd10f41d49232dec07cdcc6415f5a736c574
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-22 17:37:20 +0000

    Tue, Nov 22, 2022, 5:37 PM +00:00

---
+++ week_three_programms/fibonacci.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int f = 3;
+    int wynik = fib(f);
+    printf("%i\n", wynik);
+
+
+}
+int fib(int n)
+{
+    if (n == 1 || n == 2)
+    {
+        return 1;
+    }
+    return fib(n) + fib(n+1);
+
+}
