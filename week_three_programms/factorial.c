6219dcf33dcedc0b31ba06cb4655735568a38490
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-22 18:43:45 +0000

    Tue, Nov 22, 2022, 6:43 PM +00:00

--- week_three_programms/factorial.c
+++ week_three_programms/factorial.c
@@ -1,21 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int factorial(int n);
+int fac(int n);
 
 int main(void)
 {
 
-int fac = get_int("Enter factorial: ");
-int wynik = factorial(fac);
+int fact = get_int("Enter factorial: ");
+int wynik = fac(fact);
 printf("score: %i\n", wynik);
 
 }
-int factorial(int n)
+int fac(int n)
 {
-    if (n == 1)
+    if (n == 0)
     {
         return 1;
     }
-    return ;
+    return fac(n-1)*(fac(n-1)+1);
 }
