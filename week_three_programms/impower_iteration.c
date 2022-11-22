290f455c3da7e600368af9d91aa521153b89bb8c
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-22 17:27:43 +0000

    Tue, Nov 22, 2022, 5:27 PM +00:00

--- week_three_programms/impower_iteration.c
+++ week_three_programms/impower_iteration.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int potega(int p, int w);
+
 int main(void)
 {
 
+int p = get_int("Podaj podstawe potęgi: ");
+int w = get_int("Podaj wykładnik potęgi: ");
 
-
+int wynik = potega(p,w);
+printf("Wynik: %i\n",wynik);
 
 }
 int potega(int p, int w)
 {
-    while(w > 0)
+    int wynik = 1;
+    while (w > 0)
     {
-        int wynik = wynik*w
+    wynik = wynik*p;
+    w--;
     }
+    return wynik;
 }
