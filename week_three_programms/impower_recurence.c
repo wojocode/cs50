cce16fd20177c976040b1f3cdaa5df06f0afdfda
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-22 16:26:45 +0000

    Tue, Nov 22, 2022, 4:26 PM +00:00

--- week_three_programms/impower_recurence.c
+++ week_three_programms/impower_recurence.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <cs50.h>
 
-long potega(int w, int p);
+long potega(int p, int w);
 
 int main(void)
 {
 
+int p = 3;
+int w = 4;
 
-
+int equal= potega(w,p);
+printf("%i\n", equal);
 }
-long potega(int w, int p)
+long potega(int p, int w)
 {
     if (p == 0)
     {
         return 1;
     }
-    return potega*
+    return p*potega(w,p-1);
 }
