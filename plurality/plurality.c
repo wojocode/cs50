c081003a862d29a7e45e0d8ea91dac33e1a3cb02
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-24 18:55:45 +0000

    Thu, Nov 24, 2022, 6:55 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -73,7 +73,7 @@ bool vote(string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(name, candidates[i].name))
+        if (!strcmp(name, candidates[i].name))
         {
             candidates[i].votes++;
             return true;
