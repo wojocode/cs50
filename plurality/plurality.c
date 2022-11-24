f2ed09d04d439ec7b145359a45e43f7d29451f4c
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-24 22:38:29 +0000

    Thu, Nov 24, 2022, 10:38 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -72,11 +72,11 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    for (int i = 0; i < candidate_count; i++)
+    for (int j = 0; j < candidate_count; j++)
     {
-        if (!strcmp(name, candidates[i].name))
+        if (!strcmp(name, candidates[j].name))
         {
-            candidates[i].votes = candidates[i].votes + 1;
+            candidates[j].votes = candidates[j].votes + 1;
             return true;
         }
     }
