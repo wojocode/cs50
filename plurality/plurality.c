89078aa3338065fe9d917308673c53cdba7265ce
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-24 22:48:30 +0000

    Thu, Nov 24, 2022, 10:48 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -58,9 +58,8 @@ int main(int argc, string argv[])
         {
             printf("Invalid vote.\n");
         }
-        else {
             vote(name);
-        }
+
 
     }
     printf("%i\n",candidates[0].votes);
