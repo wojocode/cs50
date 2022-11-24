6ecd8768f1cfd270006b3f9de3286dc858e5718e
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-24 19:34:54 +0000

    Thu, Nov 24, 2022, 7:34 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -85,10 +85,10 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    for (int j = 0 )
+
     for (int i = 0; i < candidate_count; i++)
     {
-
+        
     }
 
 
