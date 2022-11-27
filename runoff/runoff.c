fe19fa05312b5a1d74bd01013c0c8af8fb85cca1
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-27 15:43:10 +0000

    Sun, Nov 27, 2022, 3:43 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -82,7 +82,7 @@ int main(int argc, string argv[])
 
         printf("\n");
     }
-  
+
 
     // Keep holding runoffs until winner exists
     while (true)
@@ -133,7 +133,7 @@ bool vote(int voter, int rank, string name)
     {
         if (!strcmp(name,candidates[i].name))
         {
-            preferences[voter][rank] = candidates[i].votes;
+            preferences[voter][rank] = i;
             return true;
         }
     }
