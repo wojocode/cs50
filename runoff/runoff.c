fa58907993be119a0328dd7c0a6fd21964890131
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-28 15:57:50 +0000

    Mon, Nov 28, 2022, 3:57 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -220,7 +220,7 @@ bool is_tie(int min)
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
-    for ( int i = 0; i < voter_count; i++)
+    for ( int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].votes == min)
         {
