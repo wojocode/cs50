c8effbef05ef077a013c7ccb567e1dede5fc4e35
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-11-28 16:04:44 +0000

    Mon, Nov 28, 2022, 4:04 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -206,7 +206,7 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    for ( int i = 0; i < voter_count; i++)
+    for ( int i = 0; i < candidate_count; i++)
     {
 // if nobody equal find min
         if (candidates[i].votes != min)
