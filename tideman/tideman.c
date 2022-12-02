a2e63f8e7820f773cd7cd94b8fab12f98608ca4e
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-02 14:36:15 +0000

    Fri, Dec 2, 2022, 2:36 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -127,8 +127,7 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    // TODO
-    return;
+    
 }
 
 // Sort pairs in decreasing order by strength of victory
