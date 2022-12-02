7d50dd8aa9dc4d602c0163da86188878775c2d57
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-02 17:18:52 +0000

    Fri, Dec 2, 2022, 5:18 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -154,7 +154,7 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-   
+
     for (int i = 0; i <= pair_count; i++)
     {
          pairs[i] = pairs[i].winner - pairs[i].loser;
