42f1d32d32f213f63a8637f91c31037ecc601656
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-03 12:47:16 +0000

    Sat, Dec 3, 2022, 12:47 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -157,7 +157,7 @@ void sort_pairs(void)
     int advantage[pair_count];
     for (int i = 0; i <= pair_count; i++)
     {
-         advantage[i] = preferences[pairs[i].winner][pairs[i].loser] - preferences[pairs[i].loser][pairs[i].winner];
+         pairs[i] = preferences[pairs[i].winner][pairs[i].loser] - preferences[pairs[i].loser][pairs[i].winner];
     }
     for (int j = 0; j < pair_count - 1; j++)
     {
