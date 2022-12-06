a900d02063f56b25acad23bf9bbd3e035dcd5b7c
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-06 16:25:39 +0000

    Tue, Dec 6, 2022, 4:25 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -116,9 +116,9 @@ void record_preferences(int ranks[])
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = 1; j <= candidate_count; j++)
+        for (int j = 0; j < candidate_count; j++)
         {
-        preferences[ranks[i]][ranks[j + i]]++;
+        preferences[ranks[i]][ranks[(j + 1) + i]]++;
         }
     }
 }
