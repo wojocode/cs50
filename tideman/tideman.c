4e3ad2a19eeced35d8b8c2035e84790a54eb9f18
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-06 22:47:49 +0000

    Tue, Dec 6, 2022, 10:47 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -195,7 +195,7 @@ void print_winner(void)
    {
         for (int j = 0; j < pair_count; j++)
         {
-                if (locked[j][i] == true)
+                if (locked[j][i] == false)
                 {
                     break;
                 }
