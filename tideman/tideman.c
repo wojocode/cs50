a30720c4464f1a7ddd36e2dc62d671b086944ddd
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-06 16:34:59 +0000

    Tue, Dec 6, 2022, 4:34 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -114,11 +114,12 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    for (int i = 0; i < candidate_count - 1; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = 0; j < candidate_count; j++)
+        
+        for (int j = 1; j <= candidate_count; j++)
         {
-        preferences[ranks[i]][ranks[(j + 1) + i]]++;
+        preferences[ranks[i]][ranks[j + i]]++;
         }
     }
 }
