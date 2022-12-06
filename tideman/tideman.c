4b4283e11945c7e7647b5b781a2e55668e027cd3
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-06 22:13:24 +0000

    Tue, Dec 6, 2022, 10:13 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -171,7 +171,10 @@ void sort_pairs(void)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    for (int i = 0; i < )
+    for (int i = 0; i < pair_count - 1; i++)
+    {
+        
+    }
 }
 
 // Print the winner of the election
