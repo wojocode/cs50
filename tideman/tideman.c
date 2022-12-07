b3a78c5d3cb9a6215a30023e197945182c8b8a9f
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-07 23:14:57 +0000

    Wed, Dec 7, 2022, 11:14 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -190,18 +190,20 @@ void lock_pairs(void)
 // Print the winner of the election
 void print_winner(void)
 {
+    int false_count[pair_count][pair_count];
    for (int i = 0; i < pair_count; i++)
    {
        for (int j = 0; j < pair_count; j++)
        {
             if (locked[j][i] == false)
             {
+               false_count[j][i]++;
             }
             else
             {
                 break;
             }
        }
-
    }
+
 }
