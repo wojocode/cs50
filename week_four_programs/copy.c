74fe95f3307c35548159d6690b2f62b9e16633e7
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-10 14:34:03 +0000

    Sat, Dec 10, 2022, 2:34 PM +00:00

--- week_four_programs/copy.c
+++ week_four_programs/copy.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 int main(void)
 {
     char *s = "hi!";
-    char *t = malloc(strlen)
+    char *t = malloc(strlen(s) + 1);
+
+    for (int i = 0 , n = strlen(s) + 1; i < n; i++)
+    {
+        t[i] = s[i];
+    }
+    t[0] = toupper(t[0]);
+    printf("%s\n",s);
+    printf("%s\n",t);
 }
---
+++ week_four_programs/copy_v2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(void)
+{
+    char *s = "hi!";
+    char *t = malloc(strlen(s+1));
+    strcpy()
+}
