5cd994337d78659d87858066112e2b797ba38a37
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-10 14:54:33 +0000

    Sat, Dec 10, 2022, 2:54 PM +00:00

--- week_four_programs/copy_v2.c
+++ week_four_programs/copy_v2.c
@@ -8,8 +8,18 @@ int main(void)
 {
     char *s = "hi!";
     char *t = malloc(strlen(s+1));
+    if (t == NULL)
+    {
+        return 1
+    }
     strcpy(t,s);
-    t[0] = toupper(t[0]);
+
+
+    if (strlen(t) > 0)
+    {
+        t[0] = toupper(t[0]);
+    }
+
     printf("%s\n",s);
     printf("%s\n",t);
 
