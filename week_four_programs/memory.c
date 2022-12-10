a6b395773a2ab12308a7219df79d332662eddaa1
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-10 15:18:39 +0000

    Sat, Dec 10, 2022, 3:18 PM +00:00

--- week_four_programs/memory.c
+++ week_four_programs/memory.c
@@ -4,8 +4,8 @@
 int main(void)
 {
  int *x = malloc(3 * sizeof(int));
-    x[1] = 72;
-    x[2] = 73;
-    x[3] = 74;
-printf("%i\n",x[100]);
+    x[0] = 72;
+    x[1] = 73;
+    x[2] = 74;
+    free(x);
 }
