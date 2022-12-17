4b4af3d4844e88892ec7fb7b89274999b02be8f6
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-17 18:10:45 +0000

    Sat, Dec 17, 2022, 6:10 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,7 +3,16 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int i = 0; i <= height - 1; i++)
+    {
+        for (int j = 0; j <= weight -1; j++)
+        {
+            image[i][j].rgbtBlue;
+            image[i][j].rgbtGreen;
+            image[i][j].rgbtRed;
+            
+        }
+    }
 }
 
 // Convert image to sepia
