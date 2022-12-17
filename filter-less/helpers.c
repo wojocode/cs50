78b74cd6de1abeb5f420bd3c4599047f97585cd8
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-17 20:59:06 +0000

    Sat, Dec 17, 2022, 8:59 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -67,7 +67,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int i = 0; i <= )
 }
 
 // Blur image
