a609bb7cabf5f0678264dec965c17e260b2b35a7
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-26 20:08:04 +0000

    Mon, Dec 26, 2022, 8:08 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -70,9 +70,8 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 
     for (int i = 0; i <= height - 1; i++)
     {
-        for (int j = 0; j <= width - 1; j++)
+        for (int j = 0; j <= ; j++)
         {
-
         RGBTRIPLE tmp = image[i][(width - 1) - j];
         image[i][(width - 1) - j] = image[i][j];
         image[i][j] = tmp;
