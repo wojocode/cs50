28dec74824a6ddd01dd05c49d412b16ee69feaef
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-27 11:02:54 +0000

    Tue, Dec 27, 2022, 11:02 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -110,9 +110,9 @@ int Blue_avr, Green_avr, Red_avr;
             //2
             else if (a == 0 && b == width - 1)
             {
-                Blue_avr = round((image[a][b].rgbtBlue + image[a][b - 1].rgbtBlue + image[a - 1][b].rgbtBlue + image[a - 1][b - 1].rgbtBlue) / 4);
-                Green_avr = round((image[a][b].rgbtGreen + image[a][b - 1].rgbtGreen + image[a - 1][b].rgbtGreen + image[a - 1][b - 1].rgbtGreen) / 4);
-                Red_avr = round((image[a][b].rgbtRed + image[a][b - 1].rgbtRed + image[a - 1][b].rgbtRed + image[a - 1][b - 1].rgbtRed) / 4);
+                Blue_avr = round((image[a][b].rgbtBlue + image[a][b - 1].rgbtBlue + image[a + 1][b].rgbtBlue + image[a + 1][b - 1].rgbtBlue) / 4);
+                Green_avr = round((image[a][b].rgbtGreen + image[a][b - 1].rgbtGreen + image[a + 1][b].rgbtGreen + image[a + 1][b - 1].rgbtGreen) / 4);
+                Red_avr = round((image[a][b].rgbtRed + image[a][b - 1].rgbtRed + image[a + 1][b].rgbtRed + image[a + 1][b - 1].rgbtRed) / 4);
             }
             //3
             else if (a == height - 1 && b == width - 1)
