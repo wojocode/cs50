d5d8aca400e6caee7beeb4bc816eb6d33380ea90
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-27 13:01:05 +0000

    Tue, Dec 27, 2022, 1:01 PM +00:00

--- week_three_programms/fibonaci_recursion.c
+++ week_three_programms/fibonaci_recursion.c
@@ -5,19 +5,24 @@ int fib(int n);
 int main(void)
 {
     int index = get_int("Enter fibonacci index: ");
-    int score = fib(index);
-    printf("%i\n", score);
+    for (int i = 0; i < index; i++)
+    {
+        int score = fib(i);
+        printf("%i,",score);
+    }
 }
-
 int fib(int n)
 {
     if (n == 1 || n ==2)
     {
         return 1;
     }
+    else if (n == 0)
+    {
+        return 0;
+    }
     else
     {
         return fib(n-2) + fib (n-1);
     }
-
 }
