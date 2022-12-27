95f0f6e2ea6782389a2b929f097052baefaea76f
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-27 19:06:22 +0000

    Tue, Dec 27, 2022, 7:06 PM +00:00

---
+++ week_four_programs/cut.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    FILE *fl = fopen("filter-less/images/yard.bmp" ,"r");
+    char ch;
+    while (ch = fgetc("filter-less/images/yard.bmp") != EOF)
+    {
+        printf("%c",ch);
+    }
+}
