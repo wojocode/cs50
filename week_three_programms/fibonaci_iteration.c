538f434ff15cee516085f0723ddcb88382acd1e0
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-27 13:43:16 +0000

    Tue, Dec 27, 2022, 1:43 PM +00:00

--- week_three_programms/fibonaci_iteration.c
+++ week_three_programms/fibonaci_iteration.c
@@ -26,7 +26,7 @@ for (int i = 0; i <= index; i++)
     {
         printf("%i,",next_numb);
         sum = numb_1 + numb_2;
-        next_numb =  + sum;
+        next_numb = next_numb + sum;
 
     }
     printf("\n");
