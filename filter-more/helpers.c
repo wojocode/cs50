6f65c7f312c67a16ddaf2f0400f364f43ada429b
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-28 14:32:34 +0000

    Wed, Dec 28, 2022, 2:32 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -23,6 +23,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+int out = -1;
 RGBTRIPLE copy[height][width];
 // 1. make copy for each pixel
     for (int i = 0; i <= height - 1; i++)
@@ -38,25 +39,25 @@ for (int i = 0; i <= height - 1 ; i++)
         for (int j = 0; j <= width - 1; j++)
         {
 //setting value 0 to pixel out of image
-//for (int a = -1; a <= width; a++)
-//{
+for (int a = -1; a <= width; a++)
+{
 
-     //image[-1][a].rgbtBlue = 0;
-     //image[-1][a].rgbtRed = 0;
-    //image[-1][a].rgbtGreen = 0;
-      //image[height][a].rgbtBlue = 0;
-      //image[height][a].rgbtRed = 0;
-      //image[height][a].rgbtGreen = 0;
-//}
-//for (int a = -1; a <= width; a++)
-//{
-      //image[a][-1].rgbtBlue = 0;
-     //image[a][-1].rgbtRed = 0;
-     //image[a][-1].rgbtGreen = 0;
-    //image[a][width].rgbtBlue = 0;
-      //image[a][width].rgbtRed = 0;
-     //image[a][width].rgbtGreen = 0;
-//}
+     image[-1][a].rgbtBlue = 0;
+     image[-1][a].rgbtRed = 0;
+    image[-1][a].rgbtGreen = 0;
+      image[height][a].rgbtBlue = 0;
+      image[height][a].rgbtRed = 0;
+      image[height][a].rgbtGreen = 0;
+}
+for (int a = -1; a <= width; a++)
+{
+      image[a][-1].rgbtBlue = 0;
+     image[a][-1].rgbtRed = 0;
+     image[a][-1].rgbtGreen = 0;
+    image[a][width].rgbtBlue = 0;
+      image[a][width].rgbtRed = 0;
+     image[a][width].rgbtGreen = 0;
+}
 
 int GX_red = round(-1*(copy[i-1][j-1].rgbtRed) + 0*(copy[i-1][j].rgbtRed) + 1*(copy[i-1][j+1].rgbtRed) + -2*(copy[i][j-1].rgbtRed) + 0*(copy[i][j].rgbtRed) + 2*(copy[i][j+1].rgbtRed) + -1*(copy[i+1][j-1].rgbtRed) + 0*(copy[i+1][j].rgbtRed) + 1*(copy[i+1][j+1].rgbtRed));
 int GX_green = round(-1*(copy[i-1][j-1].rgbtGreen) + 0*(copy[i-1][j].rgbtGreen) + 1*(copy[i-1][j+1].rgbtGreen) + -2*(copy[i][j-1].rgbtGreen) + 0*(copy[i][j].rgbtGreen) + 2*(copy[i][j+1].rgbtGreen) + -1*(copy[i+1][j-1].rgbtGreen) + 0*(copy[i+1][j].rgbtGreen) + 1*(copy[i+1][j+1].rgbtGreen));
