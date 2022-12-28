76008dad7fe2ddf021a93d6f1eb97cd571385c9d
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-28 14:21:38 +0000

    Wed, Dec 28, 2022, 2:21 PM +00:00

--- filter-more/arr.c
+++ filter-more/arr.c
@@ -3,19 +3,19 @@
 int main(void)
 {
     int arr[3][3];
+    int q = 0;
 
-for (int q = 0; q < 9; q++)
-{
     for (int i = 0; i < 3;i++)
     {
         for (int j = 0; j < 3; j++)
         {
 
             arr[i][j] = q;
+            q++;
         }
 
     }
-}
+
 for (int a = 0; a < 3;a++)
 {
     for (int b = 0; b < 3; b++)
