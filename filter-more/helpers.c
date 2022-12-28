9ece3ef48a3b30013318ed3e656b36efbcd10646
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-28 11:47:54 +0000

    Wed, Dec 28, 2022, 11:47 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -38,26 +38,26 @@ for (int i = 0; i <= height - 1 ; i++)
         for (int j = 0; j <= width - 1; j++)
         {
 //setting value 0 to pixel out of image
-for (int a = -1; a <= width; a++)
-{
-    image[-1][a].rgbtBlue = malloc(sizeof(int));
-    image[-1][a].rgbtBlue = 0;
+//for (int a = -1; a <= width; a++)
+//{
+  //  image[-1][a].rgbtBlue = malloc(sizeof(int));
+    //image[-1][a].rgbtBlue = 0;
 
-    image[-1][a].rgbtRed = 0;
-    image[-1][a].rgbtGreen = 0;
-    image[height][a].rgbtBlue = 0;
-    image[height][a].rgbtRed = 0;
-    image[height][a].rgbtGreen = 0;
-}
-for (int a = -1; a <= width; a++)
-{
-    image[a][-1].rgbtBlue = 0;
-    image[a][-1].rgbtRed = 0;
-    image[a][-1].rgbtGreen = 0;
-    image[a][width].rgbtBlue = 0;
-    image[a][width].rgbtRed = 0;
-    image[a][width].rgbtGreen = 0;
-}
+    //image[-1][a].rgbtRed = 0;
+    //image[-1][a].rgbtGreen = 0;
+    //image[height][a].rgbtBlue = 0;
+    //image[height][a].rgbtRed = 0;
+    //image[height][a].rgbtGreen = 0;
+//}
+//for (int a = -1; a <= width; a++)
+//{
+  //  image[a][-1].rgbtBlue = 0;
+    //image[a][-1].rgbtRed = 0;
+    //image[a][-1].rgbtGreen = 0;
+    //image[a][width].rgbtBlue = 0;
+    //image[a][width].rgbtRed = 0;
+    //image[a][width].rgbtGreen = 0;
+//}
 
 int GX_red = round(-1*(copy[i-1][j-1].rgbtRed) + 0*(copy[i-1][j].rgbtRed) + 1*(copy[i-1][j+1].rgbtRed) + -2*(copy[i][j-1].rgbtRed) + 0*(copy[i][j].rgbtRed) + 2*(copy[i][j+1].rgbtRed) + -1*(copy[i+1][j-1].rgbtRed) + 0*(copy[i+1][j].rgbtRed) + 1*(copy[i+1][j+1].rgbtRed));
 int GX_green = round(-1*(copy[i-1][j-1].rgbtGreen) + 0*(copy[i-1][j].rgbtGreen) + 1*(copy[i-1][j+1].rgbtGreen) + -2*(copy[i][j-1].rgbtGreen) + 0*(copy[i][j].rgbtGreen) + 2*(copy[i][j+1].rgbtGreen) + -1*(copy[i+1][j-1].rgbtGreen) + 0*(copy[i+1][j].rgbtGreen) + 1*(copy[i+1][j+1].rgbtGreen));
