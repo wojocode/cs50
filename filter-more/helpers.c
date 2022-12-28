3cf546fda5fc4df335a231b1bea689e20d9823e1
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-28 09:45:51 +0000

    Wed, Dec 28, 2022, 9:45 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -23,20 +23,20 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
 {
 RGBTRIPLE copy[height][width];
 // 1. make copy for each pixel
-    for (int i = 0; i <= height; i++)
+    for (int i = 0; i <= height - 1; i++)
     {
-        for (int j = 0; j <= width; j++)
+        for (int j = 0; j <= width - 1; j++)
         {
             copy[i][j] = image[i][j];
         }
     }
 
 // 2. assigning Gx value for each color for each pixel
-for (int i = 0; i <= height; i++)
+for (int i = 0; i <= height - 1 ; i++)
     {
-        for (int j = 0; j <= width; j++)
+        for (int j = 0; j <= width - 1; j++)
         {
-
+            
         }
     }
 }
