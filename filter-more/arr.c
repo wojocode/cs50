1e6d5842fb3a457cde67f046a64ea523005affb9
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-29 13:20:51 +0000

    Thu, Dec 29, 2022, 1:20 PM +00:00

--- filter-more/arr.c
+++ filter-more/arr.c
@@ -3,25 +3,12 @@
 
 int main(void)
 {
-    int arr[3] = {1,2,3};
+    int arr[3] = {5,6,7};
     int copy[3];
 
+  copy[0] = arr[0];
 
-        for (int j = 0; j < 3; j++)
-        {
-            int *p = &arr[j];
-            copy[j] = *p;
-        }
-        printf("%p\n %p",&copy[0],&arr[0]);
-
-        for (int i = 0;i < 3;i++)
-        {
-          printf("%i\n%i\n",copy[i],arr[i]);
-        }
-        arr[0] = 8;
-
-         for (int i = 0;i<3;i++)
-        {
-          printf("\n%i\n%i\n",copy[i],arr[i]);
-        }
+printf("%i\n%i\n",copy[0],arr[0]);
+printf("%p\n",&copy[0]);
+printf("%p\n",&arr[0]);
 }
