0935ad9d2f2a93be7e2fcc93a17c579b3c974a35
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-29 21:00:01 +0000

    Thu, Dec 29, 2022, 9:00 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -65,8 +65,60 @@ int GY_blue = round(-1*(image[a-1][b-1].rgbtBlue) + (-2*(image[a-1][b].rgbtBlue)
     + 0*(image[a][b-1].rgbtBlue) + 0*(image[a][b].rgbtBlue) + 0*(image[a][b+1].rgbtBlue)
     + 1*(image[a+1][b-1].rgbtBlue) + 2*(image[a+1][b].rgbtBlue) + 1*(image[a+1][b+1].rgbtBlue));
 
+if (GX_red <= 0)
+{
+ GX_red = 0;
+}
+else if ( GX_red > 255)
+{
+ GX_red = 255;
+}
+
+if (GX_green <= 0)
+{
+ GX_green = 0;
+}
+else if ( GX_green > 255)
+{
+ GX_green = 255;
+}
+
+if (GX_blue <= 0)
+{
+ GX_blue = 0;
+}
+else if ( GX_blue > 255)
+{
+ GX_blue = 255;
+}
 
 
+if (GY_red <= 0)
+{
+ GY_red = 0;
+}
+else if ( GX_red > 255)
+{
+ GY_red = 255;
+}
+
+if (GY_green <= 0)
+{
+ GY_green = 0;
+}
+else if ( GY_green > 255)
+{
+ GY_green = 255;
+}
+
+if (GY_blue <= 0)
+{
+ GY_blue = 0;
+}
+else if ( GY_blue > 255)
+{
+ GY_blue = 255;
+}
  //also round it, capped at 255, and allow only positive results
  long int kernel_Red = round(pow(GX_red,2) + pow(GY_red,2));
 if (kernel_Red <= 0)
