b7b6acd4c2df75dcc3224a0dd460afa1d80b1085
#Wojtek <111751570+wojocode@users.noreply.github.com>
2022-12-30 14:40:28 +0000

    Fri, Dec 30, 2022, 2:40 PM +00:00

--- filter-more/arr.c
+++ filter-more/arr.c
@@ -3,21 +3,13 @@
 
 int main(void)
 {
-    int arr[3] = {5,6,7};
-    int *copy[3];
+     typedef struct {
+      int color;
+      int width;
+      int height;
+    }
+    pixel;
+ 
 
-  copy[0] = &arr[0];
 
-
-printf("%p\n",copy[0]);
-printf("%p\n",&arr[0]);
-
-printf("%i\n",*copy[0]);
-printf("%i\n",arr[0]);
-
-int x = 802;
-int y = 802;
-
-long int z = pow(y,2) + pow(x,2);
-printf("%ld\n",z);
 }
