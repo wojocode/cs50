3a3da6ffd245553af8bae031d9bf4b4dfff50c8a
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-04 23:00:54 +0000

    Wed, Jan 4, 2023, 11:00 PM +00:00

--- license/license.c
+++ license/license.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
         buffer[6] = '\0';
 
         // Save plate number in array
-        plates[idx] = malloc(sizeof(char)*7);
+        plates[idx] = malloc(sizeof(char)*6);
         strcpy(plates[idx],buffer);
         idx++;
     }
