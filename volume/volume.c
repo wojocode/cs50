6cb2a8d8df80d2bf72a014dfb38381fc3830b41e
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-06 19:51:26 +0000

    Fri, Jan 6, 2023, 7:51 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,21 +34,21 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-
-
-
+    // rename types 
+    typedef uint8_t INFOBYTE;
+    typedef int16_t SAMPLESIZE;
     // add array to store a header from infile
-    uint8_t header[HEADER_SIZE];
-    
+    INFOBYTE header[HEADER_SIZE];
+
     fread(header,HEADER_SIZE, 1 ,input);
     fwrite(header,HEADER_SIZE, 1 ,output);
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
-    while(fread(&buffer,sizeof(int16_t),1,input))
+    while(fread(&buffer,sizeof(SAMPLESIZE),1,input))
     {
         buffer *= factor;
-        fwrite(&buffer,sizeof(int16_t),1,output);
+        fwrite(&buffer,sizeof(SAMPLESIZE),1,output);
     }
 
     // Close files
