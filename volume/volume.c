ec7e2b586ddb89663d6102aac7c98d93b6955cbb
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-06 16:03:54 +0000

    Fri, Jan 6, 2023, 4:03 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -49,7 +49,7 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
     SAMPLESIZE buffer;
-    int qty = fread()
+    int qty; = fread(&buffer,sizeof(SAMPLESIZE),qty,input);
 
 
     // Close files
