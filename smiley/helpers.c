147ffbd9e61815d83d3dd2ef6fabf5c6f0b7454d
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-06 12:16:00 +0000

    Fri, Jan 6, 2023, 12:16 PM +00:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -9,8 +9,11 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
             RGBTRIPLE pixel = image[i][j];
             if(pixel.rgbtBlue == 0 && pixel.rgbtGreen == 0 & pixel.rgbtRed == 0)
             {
-                
+                pixel.rgbtRed = 255;
+                pixel.rgbtGreen = 255;
+                pixel.rgbtBlue = 0;
             }
+            image[i][j] = pixel;
         }
     }
 }
