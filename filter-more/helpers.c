bdf352a8e1ec9debdeffb73359bb0c2add881acb
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-07 18:16:28 +0000

    Sat, Jan 7, 2023, 6:16 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -97,59 +97,59 @@ for (int a = 1; a < height - 1; a++)
             + 0*(image[a][b-1].rgbtBlue) + 0*(image[a][b].rgbtBlue) + 0*(image[a][b+1].rgbtBlue)
             + 1*(image[a+1][b-1].rgbtBlue) + 2*(image[a+1][b].rgbtBlue) + 1*(image[a+1][b+1].rgbtBlue);
 
-                //if (row[a][b].rgbtRed <= 0)
-                //{
-               // row[a][b].rgbtRed = 0;
-                //}
-                //else if ( row[a][b].rgbtRed > 255)
-               // {
-                //row[a][b].rgbtRed = 255;
-                //}
-                //if (column[a][b].rgbtRed <= 0)
-                //{
-                //column[a][b].rgbtRed = 0;
-                //}
-               // else if (column[a][b].rgbtRed > 255)
-               // {
-               // column[a][b].rgbtRed = 255;
-              //  }
-
-
-                if (row[a][b].rgbtGreen <= 0)
+                if (GX_red <= 0)
                 {
-                row[a][b].rgbtGreen = 0;
+                GX_red = 0;
                 }
-                else if (row[a][b].rgbtGreen > 255)
+                else if ( GX_red > 255)
                 {
-                row[a][b].rgbtGreen = 255;
+                row[a][b].rgbtRed = 255;
                 }
-                if (column[a][b].rgbtGreen <= 0)
+                if (GY_red <= 0)
                 {
-                column[a][b].rgbtGreen = 0;
+                GY_red = 0;
                 }
-                else if (column[a][b].rgbtGreen > 255)
+                else if (GY_red > 255)
+               {
+               GY_red = 255;
+               }
+                if (GX_green <= 0)
                 {
-                column[a][b].rgbtGreen = 255;
+                 GX_green= 0;
+                }
+                else if (GX_green > 255)
+                {
+                GX_green = 255;
+                }
+                if (GY_green <= 0)
+                {
+                GY_green = 0;
+                }
+                else if (GY_green > 255)
+                {
+                GY_green = 255;
                 }
 
-                if (row[a][b].rgbtBlue <= 0)
+                if (GX_blue <= 0)
                 {
-                row[a][b].rgbtBlue = 0;
+                GX_blue = 0;
                 }
-                else if (row[a][b].rgbtBlue > 255)
+                else if (GX_blue > 255)
                 {
-                row[a][b].rgbtBlue = 255;
+                GX_blue = 255;
                 }
-                if (column[a][b].rgbtBlue <= 0)
+                if (GY_blue <= 0)
                 {
-                column[a][b].rgbtBlue = 0;
+                GY_blue = 0;
                 }
-                else if (column[a][b].rgbtBlue > 255)
+                else if (GY_blue > 255)
                 {
-                column[a][b].rgbtBlue = 255;
+                GY_blue = 255;
                 }
 
-
+int q = GX_red;
+int z = GX_blue;
+int e = GX_green;
 
  //also round it, capped at 255, and allow only positive results
                 kernel[a][b].rgbtBlue = round(pow(GX_blue,2) + pow(GY_blue,2));
