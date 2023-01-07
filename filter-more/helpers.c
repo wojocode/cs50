ae8eef6ba0a3ac2e0127b412d7ffb289c69e6142
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-07 21:20:40 +0000

    Sat, Jan 7, 2023, 9:20 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -150,40 +150,40 @@ for (int a = 1; a < height - 1; a++)
 
 
  //also round it, capped at 255, and allow only positive results
-                int kernel_blue = round(pow(GX_blue,2) + pow(GY_blue,2));
-               // if (kernel_blue <= 0)
-            //    {
-            //   kernel_blue = 0;
-            //    }
+                int kernel_blue = pow(GX_blue,2) + pow(GY_blue,2);
+                if (kernel_blue <= 0)
+               {
+              kernel_blue = 0;
+                }
 
-              //  else if ( kernel_blue > 255)
-           //     {
-           //     kernel_blue = 255;
-           //     }
+                else if ( kernel_blue > 255)
+                {
+                kernel_blue = 255;
+               }
                     copy[a][b].rgbtRed = kernel_blue;
 
 
-                int kernel_green = round(pow(GX_green,2) + pow(GY_green,2));
-             //   if ( kernel_green <= 0)
-            //    {
-           //      kernel_green = 0;
-           //     }
-           //     else if ( kernel_green > 255)
-           //     {
-          //      kernel_green = 255;
-          //      }
+                int kernel_green = pow(GX_green,2) + pow(GY_green,2);
+                if ( kernel_green <= 0)
+                {
+                 kernel_green = 0;
+                }
+                else if ( kernel_green > 255)
+                {
+                kernel_green = 255;
+              }
                     copy[a][b].rgbtGreen = kernel_green;
 
 
-                int kernel_red= round(pow(GX_red,2) + pow(GY_red,2));
-              //  if ( kernel_red <= 0)
-             //   {
-             //   kernel_red = 0;
-             //   }
-             //   else if (kernel_red > 255)
-             //   {
-             //       kernel_red = 255;
-            //    }
+                int kernel_red= pow(GX_red,2) + pow(GY_red,2);
+               if ( kernel_red <= 0)
+               {
+               kernel_red = 0;
+              }
+              else if (kernel_red > 255)
+               {
+                   kernel_red = 255;
+               }
 
                 copy[a][b].rgbtRed = kernel_red;
 
