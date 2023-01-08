825e1c8b6901c93d10c7bd954bbfaacae19b6537
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-08 13:14:38 +0000

    Sun, Jan 8, 2023, 1:14 PM +00:00

--- filter-more/arr.c
+++ filter-more/arr.c
@@ -3,48 +3,11 @@
 #include <cs50.h>
 #include <stdint.h>
 
-typedef uint8_t  BYTE;
-typedef uint32_t DWORD;
-typedef int32_t  LONG;
-typedef uint16_t WORD;
-typedef struct
-{
-    BYTE  rgbtBlue;
-    BYTE  rgbtGreen;
-    BYTE  rgbtRed;
-} __attribute__((__packed__))
-RGBTRIPLE;
-
-
 int main(void)
 {
-RGBTRIPLE image[2][2];
-
-image[0][0].rgbtRed = 255;
-image[0][0].rgbtGreen = 255;
-image[0][0].rgbtBlue = 255;
-
-image[0][1].rgbtRed = 200;
-image[0][1].rgbtGreen = 200;
-image[0][1].rgbtBlue = 200;
-
-image[1][0].rgbtRed = 255;
-image[1][0].rgbtGreen = 255;
-image[1][0].rgbtBlue = 255;
-
-image[1][1].rgbtRed = 255;
-image[1][1].rgbtGreen = 255;
-image[1][1].rgbtBlue = 255;
-
-for (int i=0; i < 2;i++)
-{
-    for (int j = 0; j <2;j++)
-    {
-        int GX_red = image[i][j].rgbtRed + -1*image[i][j+1].rgbtRed;
-    }
-
-}
-
-
-printf("%i\n",GX_red);
+    int x = 2;
+    int y = pow(x,3);
+    printf("%i\n",y);
+    int z = sqrt(y);
+    printf("%i\n",z);
 }
