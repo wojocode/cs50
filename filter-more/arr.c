951c9090e833ee38b9bc90249333c7279f2cab69
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-08 12:28:51 +0000

    Sun, Jan 8, 2023, 12:28 PM +00:00

--- filter-more/arr.c
+++ filter-more/arr.c
@@ -18,5 +18,32 @@ RGBTRIPLE;
 
 int main(void)
 {
+RGBTRIPLE image[2][2];
 
+image[0][0].rgbtRed = 255;
+image[0][0].rgbtGreen = 255;
+image[0][0].rgbtBlue = 255;
+
+image[0][1].rgbtRed = 200;
+image[0][1].rgbtGreen = 200;
+image[0][1].rgbtBlue = 200;
+
+image[1][0].rgbtRed = 255;
+image[1][0].rgbtGreen = 255;
+image[1][0].rgbtBlue = 255;
+
+image[1][1].rgbtRed = 255;
+image[1][1].rgbtGreen = 255;
+image[1][1].rgbtBlue = 255;
+
+for (int i=0; i < 2;i++)
+for (int j=0; j <2;j++)
+{
+int GX_red = image[][0].rgbtRed - image[0][1].rgbtRed;
+}
+
+
+
+
+printf("%i\n",GX_red);
 }
