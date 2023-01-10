fd2d82c4e5bc06ac19fa7a7af165a758ef569c44
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-10 21:09:35 +0000

    Tue, Jan 10, 2023, 9:09 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 int main(int argc, char *argv[])
 {
+// initialized chunks of memory card
 const int BLOCK_SIZE = 512;
+// initialized BYTE type
+typedef uint8_t  BYTE;
+// create buffer as a array to store chunk
+BYTE buffer[512];
 
 // ensure proper usage
  if (argc != 2)
