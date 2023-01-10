1647c5f96add352b2e95da5084c4ae7a7f577029
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-10 20:27:47 +0000

    Tue, Jan 10, 2023, 8:27 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,12 +3,19 @@
 
 int main(int argc, char *argv[])
 {
-//
+// ensure proper use
  if (argc != 2)
  {
     printf("Usage: ./recover IMAGE\n");
     return 1;
  }
-  // remember filename
-    char *memory_card = argv[2];
+// remember filename
+    char *card = argv[2];
+
+//open memory card
+FILE *memory_card = fopen(card,"r");
+
+
+
+
 }
