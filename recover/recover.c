62fd6b74b5f03097ba6e73c8a6b27b3159d04573
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-11 22:17:58 +0000

    Wed, Jan 11, 2023, 10:17 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -28,7 +28,7 @@ FILE *inptr = fopen(card,"r");
         return 2;
     }
 // reading from disc image until reach the end of the file
-while (fread(//buffer,1,BLOCK_SIZE,inptr))
+while (fread(buffer, 1, BLOCK_SIZE, inptr))
 {
 
 }
