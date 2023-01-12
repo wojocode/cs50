e210701126a259d1f667424954e21b458d71e641
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-12 13:23:00 +0000

    Thu, Jan 12, 2023, 1:23 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -31,7 +31,7 @@ FILE *inptr = fopen(card,"r");
 // reading from disc image until reach the end of the file
 while (fread(buffer, 1, BLOCK_SIZE, inptr) == BLOCK_SIZE)
 {
-    fread(buffer, 1, BLOCK_SIZE, inptr);
+    
 // searching for jpg's header
     if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0 && buffer[3] == 0xe0)
         {
@@ -44,7 +44,7 @@ while (fread(buffer, 1, BLOCK_SIZE, inptr) == BLOCK_SIZE)
         }
         else
         {
-          
+
         }
 //create jpg's file and allocate them memory
 
