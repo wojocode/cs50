2d3ffe456182317f4553c4ff2b873a93fc0c18b2
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-12 22:34:24 +0000

    Thu, Jan 12, 2023, 10:34 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -35,10 +35,11 @@ while (fread(buffer, 1, BLOCK_SIZE, inptr))
 {
         char *filename = malloc(sizeof(char) * 7 + 1);
         sprintf(filename, "%03i.jpg",i);
-        FILE *img = fopen(filename,"w");
+
 // searching for jpg's header
     if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] == 0xe0 || buffer[3] == 0xe1 || buffer[3] ==0xe2 || buffer[3] ==0xe3 || buffer[3] == 0xe4 || buffer[3] == 0xe5 || buffer[3] == 0xe6 || buffer[3] == 0xe7 || buffer[3] == 0xe8 || buffer[3] == 0xe9 || buffer[3] == 0xea || buffer[3] == 0xeb || buffer[3] == 0xec || buffer[3] == 0xed || buffer[3] == 0xee || buffer[3] == 0xef))
     {
+        FILE *img = fopen(filename,"w");
         // if start of first JPEG
             if (i == 0)
             {
@@ -49,6 +50,7 @@ while (fread(buffer, 1, BLOCK_SIZE, inptr))
             }
             else
             {
+                FILE *img = fopen(filename,"w");
                 fwrite(buffer, 1, BLOCK_SIZE ,img);
                 i++;
             }
