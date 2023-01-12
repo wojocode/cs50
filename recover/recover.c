94561564e9b9de4cefae4c2ee670dda14bc355c4
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-12 12:44:22 +0000

    Thu, Jan 12, 2023, 12:44 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -33,7 +33,7 @@ while (fread(buffer, 1, BLOCK_SIZE, inptr))
 // searching for jpg's header
     if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0 && buffer[3] == 0xe0)
         {
-
+            
         }
 
 //create jpg's file and allocate them memory
