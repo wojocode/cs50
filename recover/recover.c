d90c97ab8f76b617ee14bd145cdaeb4b965f6aec
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-12 14:19:52 +0000

    Thu, Jan 12, 2023, 2:19 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -37,18 +37,19 @@ while (fread(buffer, 1, BLOCK_SIZE, inptr))
         {
             //create a file name
             char *filename = malloc(sizeof(char) * 7 + 1);
-
+            sprintf(filename, "%03i.jpg",i);
+            FILE *img = fopen(filename,"w");
+            fwrite(buffer, 1, BLOCK_SIZE,img);
 
         // if start of first JPEG
             if (i  == 0)
             {
-                sprintf(filename, "%03i.jpg",i);
-                FILE *img = fopen(filename,"w");
-                fwrite(buffer, 1, BLOCK_SIZE,img);
+                
             }
             else
             {
                 fclose(img);
+                i++;
             }
 //create jpg's file and allocate them memory
 
