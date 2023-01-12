211c1ac77206d5495d72377d1d967ef24d2f6080
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-12 13:09:04 +0000

    Thu, Jan 12, 2023, 1:09 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -35,7 +35,7 @@ while (fread(buffer, 1, BLOCK_SIZE, inptr) == BLOCK_SIZE)
     if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0 && buffer[3] == 0xe0)
         {
             //create a file name
-            char *filename = malloc(sizeof(char) * 7 + 1);
+            char *filename = malloc(sizeof(char) * 7 + 1 + 512);
             sprintf(filename, "%03i.jpg",i);
             FILE *img = fopen(filename,"w");
             fwrite(buffer, 1, BLOCK_SIZE,img);
