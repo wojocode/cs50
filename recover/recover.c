1645eef95425f8df3075506cc7338f7c842adbb1
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-12 20:20:08 +0000

    Thu, Jan 12, 2023, 8:20 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -39,7 +39,8 @@ while (fread(buffer, 1, BLOCK_SIZE, inptr))
     {
             //create a file name
             char *filename = malloc(sizeof(char) * 7 + 1);
-            
+            sprintf(filename, "%03i.jpg",i);
+            FILE *img = fopen(filename,"w");
         // if start of first JPEG
             if (i == 0)
             {
