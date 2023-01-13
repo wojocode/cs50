dda5598c4c9fc0be20308ebc3e016f8e10a67c4a
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-13 21:34:43 +0000

    Fri, Jan 13, 2023, 9:34 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -48,12 +48,10 @@ while (fread(buffer, 1, BLOCK_SIZE, inptr))
                 fwrite(buffer, 1, BLOCK_SIZE ,new);
                 i++;
                 fclose(new);
-                
                 sprintf(filename, "%03i.jpg",i);
             }
             else
             {
-
                 fwrite(buffer, 1, BLOCK_SIZE ,new);
                 i++;
 
