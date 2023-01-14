a17cbe188e0d3dfa95acd6b72154885976ae1ede
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-14 11:33:55 +0000

    Sat, Jan 14, 2023, 11:33 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+#include <stdint.h>
+#include <string.h>
+
 int main(int argc, char *argv[])
 {
- 
+// initialized chunks of memory card
+const int BLOCK_SIZE = 512;
+// initialized BYTE type
+typedef uint8_t  BYTE;
+// create buffer as a array to store chunk
+BYTE buffer[512];
+
+if (argc != 2)
+ {
+    printf("Usage: ./recover IMAGE\n");
+    return 1;
+ }
+// remember filename
+    char *card = argv[1];
+
+//open memory card
+FILE *inptr = fopen(card,"r");
+    if (inptr == NULL)
+    {
+        printf("Could not open %s.\n",card);
+        return 2;
+    }
 }
