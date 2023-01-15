89d6a1dadcefa0cb66411e4e45bd36d405992e43
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-15 16:08:51 +0000

    Sun, Jan 15, 2023, 4:08 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -34,24 +34,25 @@ while (fread(&buffer, 1, 512, inptr) == BLOCK_SIZE)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] == 0xe0 || buffer[3] == 0xe1 || buffer[3] ==0xe2 || buffer[3] ==0xe3 || buffer[3] == 0xe4 || buffer[3] == 0xe5 || buffer[3] == 0xe6 || buffer[3] == 0xe7 || buffer[3] == 0xe8 || buffer[3] == 0xe9 || buffer[3] == 0xea || buffer[3] == 0xeb || buffer[3] == 0xec || buffer[3] == 0xed || buffer[3] == 0xee || buffer[3] == 0xef))
         {
-        char *filename = malloc(8);
-        sprintf(filename, "%03i.jpg",i);
-        FILE *img = fopen(filename,"w");
-        fwrite(buffer,1,512,img);
-        fclose(img);
-        free(filename);
-        i++;
+            if(i == 0)
+            {
+                char *filename = malloc(8);
+                sprintf(filename, "%03i.jpg",i);
+                FILE *img = fopen(filename,"w");
+                fwrite(buffer,1,512,img);
+                fclose(img);
+                free(filename);
+                i++;
+            }
         }
+
         else
         {
-            printf("not found\n");
-        int a = 0;
-        a++;
-        int *b = &a;
+
 
 
 
         }
     }
-    printf("%i\n",a);
+
 }
