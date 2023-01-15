9e989ecb2541f7742ebcf52ac09bf569bd6f0b14
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-15 15:36:06 +0000

    Sun, Jan 15, 2023, 3:36 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -9,10 +9,10 @@ int main(int argc, char *argv[])
 {
 // initialized chunks of memory card
 const int BLOCK_SIZE = 512;
-// initialized BYTE type
-
+// jpges counter
+int i = 0;
 // create buffer as a array to store chunk
-BYTE buffer[BLOCK_SIZE];
+int buffer[BLOCK_SIZE];
 
 if (argc != 2)
  {
@@ -30,16 +30,20 @@ FILE *inptr = fopen(infile,"r");
         return 2;
     }
 
-fread(buffer, 1, 512, inptr);
-
-    FILE *img = fopen("1.jpg","w");
-     if (img == NULL)
+while (fread(&buffer, 1, 512, inptr) == BLOCK_SIZE)
     {
-        printf("Could not open %s.\n",infile);
-        return 3;
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] == 0xe0 || buffer[3] == 0xe1 || buffer[3] ==0xe2 || buffer[3] ==0xe3 || buffer[3] == 0xe4 || buffer[3] == 0xe5 || buffer[3] == 0xe6 || buffer[3] == 0xe7 || buffer[3] == 0xe8 || buffer[3] == 0xe9 || buffer[3] == 0xea || buffer[3] == 0xeb || buffer[3] == 0xec || buffer[3] == 0xed || buffer[3] == 0xee || buffer[3] == 0xef))
+        {
+        char *filename = malloc(8);
+        sprintf(filename, "%03i.jpg",i);
+        FILE *img = fopen(filename,"w");
+        fwrite(buffer,1,512,img);
+        fclose(img);
+        free(filename);
+        }
+        else
+        {
+
+        }
     }
-    fwrite(buffer,1,512,img);
-
-
-
 }
