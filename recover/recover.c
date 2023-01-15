01800c274c892a3817c563da625bf9e4d9ea083d
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-15 21:17:24 +0000

    Sun, Jan 15, 2023, 9:17 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -30,41 +30,40 @@ FILE *inptr = fopen(infile,"r");
         return 2;
     }
 
-while (fread(&buffer, 1, BLOCK_SIZE, inptr) == BLOCK_SIZE)
+while (fread(buffer, 1, BLOCK_SIZE, inptr) == BLOCK_SIZE)
     {
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] == 0xe0 || buffer[3] == 0xe1 || buffer[3] ==0xe2 || buffer[3] ==0xe3 || buffer[3] == 0xe4 || buffer[3] == 0xe5 || buffer[3] == 0xe6 || buffer[3] == 0xe7 || buffer[3] == 0xe8 || buffer[3] == 0xe9 || buffer[3] == 0xea || buffer[3] == 0xeb || buffer[3] == 0xec || buffer[3] == 0xed || buffer[3] == 0xee || buffer[3] == 0xef))
-        {
-            if(i == 0)
+            if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && )
             {
-                char *filename = malloc(8);
-                sprintf(filename, "%03i.jpg",i);
-                FILE *img = fopen(filename,"w");
-                fwrite(buffer,1,BLOCK_SIZE,img);
-                fclose(img);
-                free(filename);
-                i++;
+                if (i == 0)
+                {
+                    char *filename = malloc(8);
+                    sprintf(filename, "%03i.jpg",i);
+                    FILE *img = fopen(filename,"w");
+                    fwrite(buffer,1,BLOCK_SIZE,img);
+                    fclose(img);
+                    free(filename);
+                    i++;
+                }
+                else
+                {
+                    char *filename = malloc(8);
+                    sprintf(filename, "%03i.jpg",i);
+                    FILE *img = fopen(filename,"w");
+                    fwrite(buffer,1,BLOCK_SIZE,img);
+                    fclose(img);
+                    free(filename);
+                    i++;
+                }
             }
             else
             {
                 char *filename = malloc(8);
-                sprintf(filename, "%03i.jpg",i);
-                FILE *img = fopen(filename,"w");
-                fwrite(buffer,1,BLOCK_SIZE,img);
-                fclose(img);
+                sprintf(filename, "%03i.jpg",i-1);
+                FILE *img_loading = fopen(filename,"a");
+                fwrite(buffer,1,512,img_loading);
+                fclose(img_loading);
                 free(filename);
-                i++;
             }
-        }
-
-        else
-        {
-            char *filename = malloc(8);
-            sprintf(filename, "%03i.jpg",i-1);
-            FILE *img_loading = fopen(filename,"a");
-            fwrite(buffer,1,512,img_loading);
-            fclose(img_loading);
-            free(filename);
-    }
     }
 
 }
