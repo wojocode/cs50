360f5610e81a7b6092169a21c5c72b5ff57c76a2
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-17 20:28:00 +0000

    Tue, Jan 17, 2023, 8:28 PM +00:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -56,7 +56,7 @@ WAVHEADER header;
 // Use get_block_size to calculate size of block
     int block_size = get_block_size(header);
 
-    fseek(inptr, 0, SEEK_CUR);
+    fseek(inptr, -2, SEEK_END);
 
     printf("position is: %ld\n",ftell(inptr));
 
