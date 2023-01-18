9ad2f653f75634ebf4e9b0631c5b73abe4a5095c
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-18 19:36:09 +0000

    Wed, Jan 18, 2023, 7:36 PM +00:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -57,13 +57,18 @@ WAVHEADER header;
 // Use get_block_size to calculate size of block
     int block_size = get_block_size(header);
     fseek(inptr, -block_size, SEEK_END);
-    if (fseek(inptr,0,SEEK_CUR))
 
 // declare array to store each block we read in
     BYTE sample[block_size];
 
 // Write reversed audio to file
+    while (ftell(inptr) != 44)
+    {
+        
 
+
+
+    }
 
 }
 
