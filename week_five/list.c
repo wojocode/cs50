ef68f1e5889402bb2c35912bbccbfcf72cd7d3dc
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-30 19:19:22 +0000

    Mon, Jan 30, 2023, 7:19 PM +00:00

--- week_five/list.c
+++ week_five/list.c
@@ -29,4 +29,10 @@ int main(int argc, char *argv[])
         list = n;
     }
 
+    node *ptr = list;
+    while (ptr != NULL)
+    {
+        printf("%i\n",ptr->number);
+        ptr = ptr->next;
+    }
 }
