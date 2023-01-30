0bfd500df5cb26c46d9bf56578d1b9691e1eeae5
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-01-30 18:55:16 +0000

    Mon, Jan 30, 2023, 6:55 PM +00:00

--- week_five/realloc.c
+++ week_five/realloc.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+typedef struct node
+{
+    int number;
+    struct node *next;
+}
+node;
+
 int main(void)
 {
     int *number = malloc(sizeof(int) * 3);
