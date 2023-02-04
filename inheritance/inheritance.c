a03068447a8e52515c7f4eebd63751e458b84d1d
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-04 20:02:14 +0000

    Sat, Feb 4, 2023, 8:02 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -83,9 +83,10 @@ void free_family(person *p)
         return;
     }
     // TODO: Free parents recursively
-
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
     // TODO: Free child
-
+    free(p);
 }
 
 // Print each family member and their alleles.
