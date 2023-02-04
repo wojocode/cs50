dcc34e73c31259017c2e6ba5b926b3662395a63b
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-04 20:00:37 +0000

    Sat, Feb 4, 2023, 8:00 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -78,7 +78,10 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-    
+    if (p == NULL)
+    {
+        return;
+    }
     // TODO: Free parents recursively
 
     // TODO: Free child
