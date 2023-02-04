1caa45f131db563af57243779a47b242dbfc1d1f
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-04 11:44:06 +0000

    Sat, Feb 4, 2023, 11:44 AM +00:00

--- trie/trie.c
+++ trie/trie.c
@@ -110,7 +110,7 @@ int main(int argc, char *argv[])
 // TODO: Complete the check function, return true if found, false if not found
 bool check(char* word)
 {
-    
+    cursor = root;
 }
 
 // Unload trie from memory
