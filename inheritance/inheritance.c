12f192a58e7b71820e0cb0f2e79916604c672748
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-04 17:41:12 +0000

    Sat, Feb 4, 2023, 5:41 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -40,7 +40,7 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    person *p = malloc(sizeof(person));
+    person *new_p = malloc(sizeof(person));
     // If there are still generations left to create
     if (generations > 1)
     {
@@ -49,12 +49,12 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        p->parents[0] = parent0;
-        p->parents[1] = parent1;
+        new_p->parents[0] = parent0;
+        new_p->parents[1] = parent1;
+
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        
-        p->alleles[0] =
-        p->alleles[1] =
+        new_p->alleles[0] =
+        new_p->alleles[1] =
     }
 
     // If there are no generations left to create
