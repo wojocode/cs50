a7cb4a349a613756255e9790f9cf83c90c55618a
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-04 17:32:46 +0000

    Sat, Feb 4, 2023, 5:32 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -52,7 +52,7 @@ person *create_family(int generations)
         p->parents[0] = parent0;
         p->parents[1] = parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-
+        p->alleles[0] = 
     }
 
     // If there are no generations left to create
