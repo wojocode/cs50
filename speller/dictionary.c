b7403c2c96db344c5dfcf098df1920151c6a48e8
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-07 22:22:22 +0000

    Tue, Feb 7, 2023, 10:22 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -75,10 +75,6 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    if (!&load)
-    {
-        return 0;
-    }
     return words;
 }
 
