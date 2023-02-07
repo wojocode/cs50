36cfdd7a92c1d299b21ab0fc9e6ab4277bbe0ffe
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-07 12:03:17 +0000

    Tue, Feb 7, 2023, 12:03 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -36,7 +36,7 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    string word[LENGTH + 1];
+   
 // open a dictionary
    FILE *dictionary_ptr = fopen(dictionary, "r");
     if (dictionary_ptr == NULL)
