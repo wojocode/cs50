2a0c9cf936130f44822504f1f4328349168ccbcb
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-08 21:55:47 +0000

    Wed, Feb 8, 2023, 9:55 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -100,8 +100,8 @@ bool unload(void)
         {
             cursor = table[i];
             node *tmp = cursor;
-            cursor = cursor->next;
             free(tmp);
+            cursor = cursor->next;
         }
     }
     return true;
