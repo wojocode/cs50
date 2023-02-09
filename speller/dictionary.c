65b7e6b7bc70994e31106dee00f2aec982dfec62
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-09 19:06:36 +0000

    Thu, Feb 9, 2023, 7:06 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -100,10 +100,9 @@ bool unload(void)
     node *tmp;
     for (int i = 0; i < 26; i++)
     {
-
+            cursor = table[i];
             while (cursor != NULL)
             {
-            cursor = table[i];
             tmp = cursor;
             cursor= cursor->next;
             free(tmp);
