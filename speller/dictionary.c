82fcdad863126c8fc91125ea645521af65c0a719
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-10 14:47:52 +0000

    Fri, Feb 10, 2023, 2:47 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -48,9 +48,8 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-
-            return toupper(word[0]) - 'A';
-
+    return toupper(word[0]) - 'A';
+    
 }
 
 // Loads dictionary into memory, returning true if successful, else false
