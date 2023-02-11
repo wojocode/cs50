23042ac78b078f661554b1e0d3ad674bcd3f34af
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-11 18:23:48 +0000

    Sat, Feb 11, 2023, 7:23 PM +01:00

--- week_six/abstraction.py
+++ week_six/abstraction.py
@@ -1,8 +1,11 @@
-def main():
-    for i in range(3):
-        dollar()
+from cs50 import get_int
 
-def dollar():
-    print("$")
+def main():
+    x = get_int("how much  do you need ?")
+    dollar(x)
+
+def dollar(n):
+    for i in range(n):
+        print("$")
 
 main()
