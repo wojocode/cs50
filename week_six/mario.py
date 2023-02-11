a49e90e5a47b8720995598173d05060f113157d2
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-11 19:49:27 +0000

    Sat, Feb 11, 2023, 8:49 PM +01:00

--- week_six/mario.py
+++ week_six/mario.py
@@ -8,7 +8,10 @@ def main():
 
 def get_height():
     while True:
-        n = int(input("Height: "))
-        if n > 0:
-            return n
+        try:
+            n = int(input("Height: "))
+            if n > 0:
+                return n
+        except ValueError:
+            print("Invalid format")
 main()
