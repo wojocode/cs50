c86939da0123ab6cce892c6f0c27b83b1a8975b0
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-11 17:54:20 +0000

    Sat, Feb 11, 2023, 6:54 PM +01:00

--- week_six/compare.py
+++ week_six/compare.py
@@ -1,12 +1,11 @@
 # Logical operators
-
 from cs50 import get_string
 
 # Prompt user to agree
 s = get_string("Do you agree? ")
-
+x = s.lower()
 # Check whether agreed
-if s == "Y" or s == "y":
+if x in ["yes" , "y"]:
     print("Agreed.")
-elif s == "N" or s == "n":
+elif x in ["no","n"]:
     print("Not agreed.")
