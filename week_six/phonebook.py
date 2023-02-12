6e2852e70efb5c78e58bdf15a3a4d51dd01ad106
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-12 17:02:09 +0000

    Sun, Feb 12, 2023, 6:02 PM +01:00

--- week_six/phonebook.py
+++ week_six/phonebook.py
@@ -1,10 +1,9 @@
 people = {
-    "Wojtek": "660-511-677",
-    "Mary": "696-048-675"
-}
+    "Wojtek": "660-511-677", "Mary": "696-048-675"
+          }
 name = input("name: ")
 
 if name in people:
-    print("found")
+    print(f"Number: {people[name]}")
 else:
     print("not found")
