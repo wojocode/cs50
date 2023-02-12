ff2bc445ef6ec045c603a22bdbfa30ce2ab34700
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-12 23:21:21 +0000

    Mon, Feb 13, 2023, 12:21 AM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -13,8 +13,8 @@ figlet.getFonts()
 arg = len(sys.argv)
 if arg == 1:
     plaintext = input("Input: ")
-
-    figlet.setFont(font=random.choice(figlet))
+    ascii = random.shuffle(figlet)
+    #figlet.setFont(font={random.choices(figlet)})
     print(figlet.renderText(plaintext))
 
 
