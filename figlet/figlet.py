28a0439ed82dec3b77171097a7a362526cc036a5
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-12 22:51:04 +0000

    Sun, Feb 12, 2023, 11:51 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -8,13 +8,12 @@ arg = len(sys.argv)
 if arg != 0 and arg != 2:
     print("invalid usage")
     sys.exit(1)
-'''
-if arg == 1:
 
-elif arg == 2:
-    if sys.argv[2] == "-f" or "--font":
+
+
+
+if sys.argv[1] == "-f" or "--font":
         sys.exit(0)
-    else:
-        print("invalid usage")
-
-'''
+else:
+    print("invalid usage")
+    sys.exit(1)
