2938d877c9ed9ff81eef0b9305577d00ae07a980
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-12 20:28:33 +0000

    Sun, Feb 12, 2023, 9:28 PM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,7 +1,7 @@
 
 while True:
     greet = input("Greeting: ")
-    if greet.isascii() and greet.isalnum() != True:
+    if greet.isascii() and greet.isalnum():
         break
 
 greet = greet.lower()
