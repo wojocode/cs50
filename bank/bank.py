edacde8c857c1c20feff0ebc531be891eb6b4955
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-12 19:52:36 +0000

    Sun, Feb 12, 2023, 8:52 PM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,7 +1,12 @@
+
 while True:
     greet = input("Greeting: ")
-    if greet
-
+    if greet.isalpha():
+        break
 
 if greet.lower() != "hello":
     print("100")
+elif greet[0].lower() == "h":
+    print("20")
+else:
+    print("0")
