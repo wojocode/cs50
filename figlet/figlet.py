5dd97a5bcf6ae923153bf521b7528ca4a1fc8c7c
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-12 23:33:30 +0000

    Mon, Feb 13, 2023, 12:33 AM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -7,17 +7,17 @@ from pyfiglet import Figlet
 figlet = Figlet()
 
 #get a list
-figlet.getFonts()
-length = len(figlet.getFonts())
+font_list = figlet.getFonts()
 
-print(f"{length}")
+length = len(font_list)
+
+
 arg = len(sys.argv)
 if arg == 1:
     plaintext = input("Input: ")
     ascii = random.shuffle(figlet)
     #figlet.setFont(font={random.choices(figlet)})
     print(figlet.renderText(plaintext))
-
 
 if arg == 2:
     if (sys.argv[1] == "-f" or sys.argv[1] == "--f"):
