a2fda321ef22c3cad9c113ff8f9c7a30ac358c5f
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-12 22:46:13 +0000

    Sun, Feb 12, 2023, 11:46 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -4,6 +4,12 @@ from pyfiglet import Figlet
 
 figlet = Figlet()
 
+arg = len(sys.argv)
+if arg == 1:
 
-if len(sys.argv) == 0:
-    print("no")
+elif arg == 2:
+    if sys.argv[2] == "-f" or "--font":
+        sys.exit(0)
+    else:
+        print("invalid usage")
+
