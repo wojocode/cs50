82d7ac4248d7ab6a70baada59e698a199b3a4442
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-14 21:40:50 +0000

    Tue, Feb 14, 2023, 10:40 PM +01:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -19,12 +19,11 @@ def main():
         with open(sys.argv[1]) as file:
             reader = csv.DictReader(file,fieldnames=["team", "rating"])
             for row in reader:
-                row = row[team]
                 teams.append(row)
     except FileNotFoundError:
         print("FILENAME is not correct")
 
-
+    print(f"te: {teams}")
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
 
