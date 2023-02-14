f8c16c0393c90c3f3282e3f6fb28a2def2c8d019
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-14 21:05:54 +0000

    Tue, Feb 14, 2023, 10:05 PM +01:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -14,6 +14,7 @@ def main():
     if len(sys.argv) != 2:
         sys.exit("Usage: python tournament.py FILENAME")
 
+# appending teams from file
     teams = []
     with open(sys.argv[1]) as file:
       reader = csv.DictReader(file)
