d587189be7d5397cb8b95e1c50157b39870fa0cb
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-15 21:10:13 +0000

    Wed, Feb 15, 2023, 10:10 PM +01:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -21,7 +21,7 @@ def main():
 
             for row in reader:
                 list(row)
-               
+                get(row[])
                 teams.append(row)
 
     except FileNotFoundError:
