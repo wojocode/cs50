f8b9d3dc1ae8be93d6f595f5fe2de0a8f2056bf1
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-15 20:56:08 +0000

    Wed, Feb 15, 2023, 9:56 PM +01:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -20,7 +20,7 @@ def main():
             reader = csv.DictReader(file)
 
             for row in reader:
-                int(row[])
+                
                 teams.append(row)
 
     except FileNotFoundError:
