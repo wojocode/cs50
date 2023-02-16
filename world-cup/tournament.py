dd11d48614213630753b44c9e2e845b2c429efa3
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-16 15:56:35 +0000

    Thu, Feb 16, 2023, 4:56 PM +01:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -26,14 +26,13 @@ def main():
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
-    #while True:
-    while True:
-        simulate_round(teams)
-        if len(simulate_round(teams)) == 8:
-            print("koniec")
-            break
-        #if len(winners) == 2:
-           # break
+
+    one_eight_final = simulate_round(teams)
+    one_fourth_cup = simulate_round(one_eight_final)
+    semi_final = simulate_round(one_fourth_cup)
+    final = simulate_round(semi_final)
+    counts.append
+
 
 
 
