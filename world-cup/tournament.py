94b8fe75d9bd01c71176d1b94a419efd370a8a33
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-17 15:23:39 +0000

    Fri, Feb 17, 2023, 4:23 PM +01:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -32,7 +32,7 @@ def main():
         team = simulate_tournament(teams)
         if team in counts:
 
-            counts.update({team:(x)}) = counts.update({team:(x)}) + counts.update({team:(x)})
+            counts = counts.update({team:(x)}) + counts.update({team:(x)})
         else:
             counts.update({team:y})
     print(counts)
