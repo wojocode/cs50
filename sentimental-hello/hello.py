d9254fe6215ec740c0800c225353d86b54f2d8fb
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-19 22:10:00 +0000

    Sun, Feb 19, 2023, 11:10 PM +01:00

--- sentimental-hello/hello.py
+++ sentimental-hello/hello.py
@@ -1,10 +1,4 @@
 
-while True:
-    name = input("What is your name ? \n")
+name = input("What is your name ? \n")
 
-    if name.isnumeric == True:
-         print("okay")
-         continue
-    else:
-        break
 print(f"hello", name)
