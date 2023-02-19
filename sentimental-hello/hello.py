af812bd2099133bb7acc03603801fe15c176a003
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-19 21:42:24 +0000

    Sun, Feb 19, 2023, 10:42 PM +01:00

--- sentimental-hello/hello.py
+++ sentimental-hello/hello.py
@@ -1,10 +1,8 @@
-
-try:
+while True:
     name = input("What is your name ? \n")
     if name.isalpha:
         print(f"hello", name)
-
-except name.isalpha != True:
-    print("only string input")
+        break
+    
 
 
