247ee2778bc0a726bccb5d41ceb0c25a94774a53
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-20 14:14:10 +0000

    Mon, Feb 20, 2023, 3:14 PM +01:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -10,7 +10,7 @@ for i in range(height):
     for a in range(1,height - i):
         print(" ",end="")
 
-    for b in range(height - a):
+    for b in range(height):
         print("%",end="")
 
     print("",end ='\n')
