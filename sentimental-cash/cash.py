8b3c548f2f502c0eab63744b55e2813443f8b77c
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-20 21:21:41 +0000

    Mon, Feb 20, 2023, 10:21 PM +01:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -9,5 +9,13 @@ while True:
 
 
 def quarters(cents):
-    return cents /25
+    return ( cents * 100) / 25
 
+def dimes(cents):
+    return ( cents * 100) / 10
+
+def nickels(cents):
+    return ( cents * 100) / 5
+
+def pennies(cents):
+    return ( cents * 100) / 1
