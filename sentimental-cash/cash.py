9a7c7beed2c5b17b9a777bee41cdadbe52976fc4
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-20 21:28:42 +0000

    Mon, Feb 20, 2023, 10:28 PM +01:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,12 +1,5 @@
 from cs50 import get_float
 
-#prompt user
-while True:
-    cents = get_float("Change owed: ")
-    if cents > 0:
-        break
-
-
 
 def quarters(cents):
     return ( cents * 100) / 25
@@ -19,3 +12,25 @@ def nickels(cents):
 
 def pennies(cents):
     return ( cents * 100) / 1
+
+
+#prompt user
+while True:
+    cents = get_float("Change owed: ")
+    if cents > 0:
+        break
+
+quarters_ = quarters(cents)
+cents = cents - (quarters_ * 25)
+
+dimes_ = dimes(cents)
+cents = cents - (dimes_ * 10)
+
+nickels_ = nickels(cents)
+cents = cents - (nickels_ * 10)
+
+pennies_ = pennies(cents)
+cents = cents - (pennies_ * 1)
+
+sum = quarters_ + dimes_ + nickels_ + pennies_
+print(sum)
