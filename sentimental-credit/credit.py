3f9d87548003bbed7d4d33e4db9481b4c88b74be
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-21 18:52:25 +0000

    Tue, Feb 21, 2023, 7:52 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,30 +2,15 @@ import re
 from cs50 import get_int, get_string
 
 
-#prompt user
-while True:
-    card_number = get_int("enter card number: ")
-    if card_number > 0:
-        break
 
-def checksum(card_number):
-#checking if number length is add or even
-    length = len(str(card_number))
-    m = 100
-    if length % 2 == 0:
-        for i in range(length / 2):
-            digit = (card_number & m) * 2
-            m = m * 10
-            sum = sum + digit
-    else:
-        for i in range(round((length / 2)) - 1):
-            digit = (card_number & m) * 2
-            m = m * 10
-            sum = sum + digit
+
+x= 5
+y = 2
+print(round(x/y))
 
 
 
-checksum(card_number)
+#checksum(card_number)
 # convert int to strin (len(str(card_number)))
 
 
