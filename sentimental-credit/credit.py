24895a994b15501404ecb31f3a685bd3ce5334d2
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-21 16:30:13 +0000

    Tue, Feb 21, 2023, 5:30 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,8 +3,12 @@ from cs50 import get_int
 #prompt user
 while True:
     card_number = get_int("enter card number: ")
-    if card_number > 0 and str(card_number)[0] != 0:
+    if card_number > 0:
         break
 
 # convert int to strin (len(str(card_number)))
-#def checksum(card_number):
+def checksum(card_number):
+    
+    card_length = (len(str(card_number)))
+    for i in range(1, card_length + 1):
+
