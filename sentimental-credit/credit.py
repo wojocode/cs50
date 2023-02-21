08ad5e25ebe280860008f86f4246cfd2a06a525a
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-21 17:28:37 +0000

    Tue, Feb 21, 2023, 6:28 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,11 +1,11 @@
 import re
-from cs50 import get_int
+from cs50 import get_int, get_string
 
 pattern = "0"
 #prompt user
 while True:
     card_number = get_int("enter card number: ")
-    if card_number > 0 and re.search(pattern, str(card_number)):
+    if  re.search(pattern, str(card_number)) != None:
         break
 
 # convert int to strin (len(str(card_number)))
