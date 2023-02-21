4cad7ce14e1b668a6f9284e3ad72da3ad0b37b97
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-21 18:23:47 +0000

    Tue, Feb 21, 2023, 7:23 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,13 +4,11 @@ from cs50 import get_int, get_string
 
 #prompt user
 while True:
-
     card_number = get_string("enter card number: ")
-
-    if card_number.isalpha:
+    if card_number.isdigit:
         break
 
-
+        
 
 # convert int to strin (len(str(card_number)))
 #def checksum(card_number):
