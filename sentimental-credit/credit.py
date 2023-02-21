f0198eb7ebc2e2a0fddb6794440126716ef95b0f
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-21 17:48:55 +0000

    Tue, Feb 21, 2023, 6:48 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -6,6 +6,7 @@ from cs50 import get_int, get_string
 while True:
 
     card_number = get_int("enter card number: ")
+    print(str(card_number))
     if re.match("^0", str(card_number)):
         continue
     else:
