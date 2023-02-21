da684897e6bec38cdf18fc11f1f285728f78bd7e
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-21 17:31:18 +0000

    Tue, Feb 21, 2023, 6:31 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -5,7 +5,9 @@ pattern = r"0"
 #prompt user
 while True:
     card_number = get_int("enter card number: ")
-    if  re.search(pattern, str(card_number)) == None:
+    if  re.search(pattern, str(card_number)) != None:
+        continue
+    else:
         break
 
 # convert int to strin (len(str(card_number)))
