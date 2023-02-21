cd0578525c7620475b491cb983df80ab56c89fe6
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-21 16:24:39 +0000

    Tue, Feb 21, 2023, 5:24 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,7 +3,7 @@ from cs50 import get_int
 #prompt user
 while True:
     card_number = get_int("enter card number: ")
-    if card_number > 0:
+    if card_number > 0 and card_number % (len(str(card_number))):
         break
 
 # convert int to strin (len(str(card_number)))
