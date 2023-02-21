4a50bcc773110f463c4fc5f9e395cbfb68091915
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-21 18:30:28 +0000

    Tue, Feb 21, 2023, 7:30 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,9 +4,8 @@ from cs50 import get_int, get_string
 
 #prompt user
 while True:
-    card_number = get_string("enter card number: ")
-    card = card_number.isdigit
-    if card == True:
+    card_number = get_int("enter card number: ")
+    if card_number > 0:
         break
 
 # convert int to strin (len(str(card_number)))
