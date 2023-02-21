1f141f089a8d506686302e4b70dd67a8a9eae9c6
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-21 21:41:47 +0000

    Tue, Feb 21, 2023, 10:41 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -22,54 +22,54 @@ def checksum(card_number):
     first_sum = sum
 
 # getting every other digit multiplying by 2
-        multiplier = 100
-        sum = 0
-        divider = 10
-        tens = 0
-        units = 0
-        for j in range(length - 1, 0 , -2):
-            digit = int((card_number % multiplier) / divider) * 2
-            if digit >= 10:
-                tens = int(digit / 10)
-                units = int(digit % 10)
-                sum = sum  + tens + units
-                multiplier = multiplier * 100
-                divider = divider * 100
-                tens = 0
-                units = 0
-            else:
-                sum = sum + digit + tens + units
-                multiplier = multiplier * 100
-                divider = divider * 100
-        second_sum = sum
-
-    # add sums
-        result = first_sum + second_sum
-
-    #checking sum
-        if result % 10 == 0:
-            return True
+    multiplier = 100
+    sum = 0
+    divider = 10
+    tens = 0
+    units = 0
+    for j in range(length - 1, 0 , -2):
+        digit = int((card_number % multiplier) / divider) * 2
+        if digit >= 10:
+            tens = int(digit / 10)
+            units = int(digit % 10)
+            sum = sum  + tens + units
+            multiplier = multiplier * 100
+            divider = divider * 100
+            tens = 0
+            units = 0
         else:
-            return False
+            sum = sum + digit + tens + units
+            multiplier = multiplier * 100
+            divider = divider * 100
+    second_sum = sum
+
+# add sums
+    result = first_sum + second_sum
+
+#checking sum
+    if result % 10 == 0:
+        return True
+    else:
+        return False
 
 
-    final = checksum(card_number)
+final = checksum(card_number)
 
-    if final != True:
-        print("INVALID")
-        sys.exit(1)
+if final != True:
+    print("INVALID")
+    sys.exit(1)
 
-    #  convert int to string
-    number = str(card_number)
+#  convert int to string
+number = str(card_number)
 
-    # AMERICAN EzXPRESS
-    if len(number) == 15 and number[0] == '3' and (number[1] == '7' or number[1] == '4'):
-            print("AMEX")
+# AMERICAN EzXPRESS
+if len(number) == 15 and number[0] == '3' and (number[1] == '7' or number[1] == '4'):
+        print("AMEX")
 
-    # MASTER CARD
-    if len(number) == 16 and number[0] == '5' and (number[1] == '1' or number[1] == '2' or number[1] == '3' or number[1] == '4' or number[1] == '5'):
-            print("MASTERCARD")
+# MASTER CARD
+if len(number) == 16 and number[0] == '5' and (number[1] == '1' or number[1] == '2' or number[1] == '3' or number[1] == '4' or number[1] == '5'):
+        print("MASTERCARD")
 
-    # VISA
-    if len(number) == 13 and number[0] == '4' or len(number) == 16 and number[0] == '4':
-            print("VISA")
+# VISA
+if len(number) == 13 and number[0] == '4' or len(number) == 16 and number[0] == '4':
+        print("VISA")
