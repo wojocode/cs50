6471e5b360a6e3facd4085c2b972c4f7ac050b96
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-21 18:14:52 +0000

    Tue, Feb 21, 2023, 7:14 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -6,14 +6,14 @@ from cs50 import get_int, get_string
 while True:
 
     card_number = get_int("enter card number: ")
-    
+    print(len(str(card_number)))
     if card_number > 0:
         break
 
 
 
 # convert int to strin (len(str(card_number)))
-def checksum(card_number):
-    card_length = (len(str(card_number)))
-    #for i in range(1, card_length + 1):
+#def checksum(card_number):
+    #checking if number length is add or even
+
 
