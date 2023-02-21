350912870c8ca54722db07aeeed27328af8f9252
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-21 19:59:37 +0000

    Tue, Feb 21, 2023, 8:59 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -9,13 +9,13 @@ while True:
 def checksum(card_number):
 #checking if number length is add or even
     length = len(str(card_number))
-    multiplier = 1
+    multiplier = 10
     sum = 0
     divider = 1
 
     for i in range(length, 0 , -2):
-        multiplier = multiplier * 100
         digit = int((card_number % multiplier) / divider)
+        multiplier = multiplier * 100
         sum = sum + digit
         divider = divider * 100
 
