955ca45677757153a8198b4fbc8063be555baf7e
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-22 21:25:38 +0000

    Wed, Feb 22, 2023, 10:25 PM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,7 +1,16 @@
 # prompt user for string
 
-text = input("Text: ").islower
-if text:
-    print("okay")
-else:
-    print("not okay")
+while True:
+    text = input("Text: ")
+    if len(text) == 0:
+        continue
+    else:
+        break
+# letters counter
+
+letters = 0
+for i in range(len(text)):
+    if text[i].isalpha:
+        letters += 1
+
+print(letters)
