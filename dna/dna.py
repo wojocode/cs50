04738cb79a388df4a47acd900636059e272b8add
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-25 21:53:11 +0000

    Sat, Feb 25, 2023, 10:53 PM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -43,8 +43,12 @@ def main():
 
 
     # TODO: Check database for matching profiles
-    
-
+    for j in range(0,len(database_list)):
+        for i in range(1,len(key_list)):
+            if int(STR_list[i-1]) != int(database_list[j][key_list[i]]):
+                break
+            else:
+                continue
 
 
 
