4f845241ce95ddba6ad4580f1626beca3cfb8f27
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-27 19:38:41 +0000

    Mon, Feb 27, 2023, 8:38 PM +01:00

--- week_six/email_regex.py
+++ week_six/email_regex.py
@@ -1,6 +1,6 @@
 import re
 email = input("type your email: ")
-if re.match("[a-z A-Z 0-9 /_/./-]+[@][a-z 0-9][/.][a-z]{2,3}", email):
+if re.match("[a-z A-Z 0-9 /_/./-]+[@][a-z]+[/.][a-z]{2,3}", email):
     print("correct email")
 else:
     print("incorrect email")
