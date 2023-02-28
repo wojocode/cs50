1fcfa2942155c8eef41788a6047e35e4db4537af
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-28 17:38:15 +0000

    Tue, Feb 28, 2023, 6:38 PM +01:00

--- week_seven/preference.py
+++ week_seven/preference.py
@@ -2,13 +2,12 @@ import csv
 
 with open("preference.csv" , "r") as file:
     reader = csv.DictReader(file)
-    count = 0
-    di = {}
+    counts = dict()
     for row in reader:
         if row["language"] in reader:
-            count += 1
-            di["language"] = count
+            counts["language"]+= 1
+
         else:
-            count = 1
-            di["language"] = count
-print(di)
+            counts["language"] = 1
+
+print(counts)
