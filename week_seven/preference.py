bf5cc64990dae1b367bfc37a413c15f3cd6d5a09
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-02-28 17:41:28 +0000

    Tue, Feb 28, 2023, 6:41 PM +01:00

--- week_seven/preference.py
+++ week_seven/preference.py
@@ -4,8 +4,8 @@ with open("preference.csv" , "r") as file:
     reader = csv.DictReader(file)
     counts = dict()
     for row in reader:
-        if row["language"] in reader:
-            favorits = row["language"]
+        favorits = row["language"]
+        if favorits in reader:
             counts[favorits]+= 1
         else:
             counts[favorits] = 1
