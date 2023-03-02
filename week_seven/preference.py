2dc0801960d4f28f4d8a99f885628da9311ebc2a
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-02 19:08:11 +0000

    Thu, Mar 2, 2023, 8:08 PM +01:00

--- week_seven/preference.py
+++ week_seven/preference.py
@@ -5,4 +5,10 @@ db = SQL("sqlite:///database.db")
 
 favorite = input("problem: ")
 
+rows = db.execute("SELECT * from ps WHERE problem='Mario'")
+
+for row in rows:
+    print(row["language"])
+
+
 
