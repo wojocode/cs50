a92abb13275803ca1ff4110c4378e772e90e7adf
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-04 18:37:59 +0000

    Sat, Mar 4, 2023, 7:37 PM +01:00

--- favorites/favorites.sql
+++ favorites/favorites.sql
@@ -1,2 +1,6 @@
 UPDATE shows SET title = 'The Office' WHERE title LIKE "the%office%";
 UPDATE shows SET title = 'Brooklyn Nine-Nine' WHERE title LIKE "%Brooklyn%";
+UPDATE shows SET title = 'Game of Thrones' WHERE title LIKE "%Game%of%Thrones%";
+UPDATE shows SET title = 'Grey’s Anatomy' WHERE title LIKE "%Grey’s%Anatomy%";
+UPDATE shows SET title = 'It’s Always Sunny in Philadelphia' WHERE title LIKE "%It’s%Always%Sunny%in%Philadelphia%";
+UPDATE shows SET title = 'Parks and Recreation' WHERE title LIKE "%Parks%and%Recreation%";
