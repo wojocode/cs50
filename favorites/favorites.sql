50a1c34f1ef624967dcd0676e25dc1eebb7bd47f
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-04 18:27:41 +0000

    Sat, Mar 4, 2023, 7:27 PM +01:00

--- favorites/favorites.sql
+++ favorites/favorites.sql
@@ -1,2 +1,2 @@
 UPDATE shows SET title = 'The Office' WHERE title = LIKE "the%office%";
-UPDATE shows SET title = 'Brooklyn Nine-Nine' WHERE title = LIKE "%Brooklyn%Nine-Nine%";
+UPDATE shows SET title = 'Brooklyn Nine-Nine' WHERE title = LIKE "%Brooklyn%";
