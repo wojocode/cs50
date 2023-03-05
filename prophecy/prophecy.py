9c4eb1b2e91e974df1260ef0e5a1b015a147590d
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-05 20:29:59 +0000

    Sun, Mar 5, 2023, 9:29 PM +01:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -1,2 +1,12 @@
 from cs50 import SQL
 
+#open old database
+old_db = SQL("sqlite:///roster.db")
+#open new database
+new_db = SQL("sqlite:///ros.db")
+
+#get student_name from old database
+student_name = old_db.execute("SELECT student_name FROM students")
+#insert student_name to new table
+for row in student_name:
+    new_db.execute("INSERT student_name FROM students")
