2b3a7b23c3c67756b33959e8e0d2d49d7cac0d7a
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-05 22:32:18 +0000

    Sun, Mar 5, 2023, 11:32 PM +01:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -1,2 +1,11 @@
 from cs50 import SQL
 
+import csv
+
+try:
+        with open("students.csv", "r") as file:
+            reader = csv.DictReader(file)
+            for team in reader:
+                print(team)
+except FileNotFoundError:
+        print("FILENAME is not correct")
