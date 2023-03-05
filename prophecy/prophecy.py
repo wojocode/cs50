da47067a8530fec4eeaa5a603972e614581ced57
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-05 21:06:10 +0000

    Sun, Mar 5, 2023, 10:06 PM +01:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -12,4 +12,4 @@ for row in student_name:
     new_db.execute("INSERT INTO students (student_name) VALUES(?)",row['student_name'])
 
 #insert house_name to new table
-new_db.execute("INSERT INTO houses (house) VALUES (Gryffindor)")
+new_db.execute("INSERT INTO houses (house) VALUES (G)")
