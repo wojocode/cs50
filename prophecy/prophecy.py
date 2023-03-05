c3b7246221cbffd64f33b2b8923845f6d298c0e7
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-05 22:35:18 +0000

    Sun, Mar 5, 2023, 11:35 PM +01:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -2,10 +2,12 @@ from cs50 import SQL
 
 import csv
 
+db = SQL("sqlite:///ros.db")
+
 try:
         with open("students.csv", "r") as file:
             reader = csv.DictReader(file)
             for team in reader:
-                print(team)
+                team['student_name']
 except FileNotFoundError:
         print("FILENAME is not correct")
