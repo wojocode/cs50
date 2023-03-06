9dff71ef85b959bc4c10c72692b37e9f9a766b91
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-06 20:52:31 +0000

    Mon, Mar 6, 2023, 9:52 PM +01:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -7,14 +7,14 @@ new_db = SQL("sqlite:///database.db")
 #select students_name
 rows = db.execute("SELECT student_name from students")
 #inser student_name
-#for row in rows:
-        #new_db.execute("INSERT INTO students (student_name) VALUES (?)",row['student_name'])
+for row in rows:
+        new_db.execute("INSERT INTO students (student_name) VALUES (?)",row['student_name'])
 
 #select houses
 rows = db.execute("SELECT house FROM students GROUP BY house")
 #insert houses
-#for row in rows:
-     #   new_db.execute("INSERT INTO houses (house) VALUES (?)",row['house'])
+for row in rows:
+     new_db.execute("INSERT INTO houses (house) VALUES (?)",row['house'])
 
 #select student
 rows = db.execute("SELECT id,house from students")
