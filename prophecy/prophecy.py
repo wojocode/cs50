d653338ceaac7f3fbfb50913d3efc0d1d2fd9f2c
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-06 19:11:56 +0000

    Mon, Mar 6, 2023, 8:11 PM +01:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -2,3 +2,8 @@ from cs50 import SQL
 
 db = SQL("sqlite:///roster.db")
 
+new_db = SQL("sqlite:///database.db")
+
+rows = db.execute("SELECT student_name from students")
+for row in rows:
+    print(row)
