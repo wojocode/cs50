83f6f57cfc49b6545864907de1d0d33239034475
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-06 19:36:32 +0000

    Mon, Mar 6, 2023, 8:36 PM +01:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -3,7 +3,7 @@ from cs50 import SQL
 db = SQL("sqlite:///roster.db")
 
 new_db = SQL("sqlite:///database.db")
-
+'''
 #select students_name
 rows = db.execute("SELECT student_name from students")
 #inser student_name
@@ -15,6 +15,10 @@ rows = db.execute("SELECT house FROM students GROUP BY house")
 #insert houses
 for row in rows:
         new_db.execute("INSERT INTO houses (house) VALUES (?)",row['house'])
+'''
 
-
-
+#select student
+rows = db.execute("SELECT id from students")
+#insert student
+for row in rows:
+        new_db.execute("INSERT INTO house_assignment (student_id) VALUES (?)",row['id'])
