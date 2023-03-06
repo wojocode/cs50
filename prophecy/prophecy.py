c77475310894f987bf18a7711343a767eead83ac
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-06 19:06:19 +0000

    Mon, Mar 6, 2023, 8:06 PM +01:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -1,16 +1,4 @@
 from cs50 import SQL
 
-import csv
+db = SQL("sqlite:///roster.db")
 
-db = SQL("sqlite:///database.db")
-
-try:
-        with open("students.csv", "r") as file:
-            reader = csv.DictReader(file)
-
-            for team in reader:
-                db.execute("INSERT INTO students (student_name) VALUES (?)",team['student_name'])
-except FileNotFoundError:
-        print("FILENAME is not correct")
-
-db.execute("INSERT INTO houses (house,house,house,house) VALUES ('Slytherin','Ravenclaw','Gryffindor','Hufflepuff')")
