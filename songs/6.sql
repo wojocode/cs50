3faf9d7546a09a86a6709b1ef3c801b19e8a0df2
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-07 22:11:15 +0000

    Tue, Mar 7, 2023, 11:11 PM +01:00

--- songs/6.sql
+++ songs/6.sql
@@ -1,1 +1,1 @@
-SELECT name FROM songs WHERE artist_id =(SELECT id FROM artists WHERE artists.name = "Post Malone");
+SELECT name FROM songs WHERE artist_id = (SELECT id FROM artists WHERE name = "Post Malone");
