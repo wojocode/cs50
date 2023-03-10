aaaff39829944866bff00c9379d05f7cfefd9461
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-10 15:45:06 +0000

    Fri, Mar 10, 2023, 4:45 PM +01:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,1 +1,3 @@
-SELECT name FROM people WHERE id in (SELECT person_id FROM stars WHERE movie_id in (SELECT id FROM movies WHERE title = "Toy Story"));
+SELECT name FROM people WHERE id
+    IN (SELECT person_id FROM stars WHERE movie_id
+        IN (SELECT id FROM movies WHERE title = "Toy Story"));
