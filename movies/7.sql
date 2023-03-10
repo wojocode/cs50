f41c98201c3947114d9d69f889cb4227574be4b1
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-10 15:27:07 +0000

    Fri, Mar 10, 2023, 4:27 PM +01:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,2 +1,3 @@
 SELECT movies.title , ratings.rating FROM movies
-    JOIN ratings ON ratings.movie_id = movies.id WHERE year = 2010 ORDER BY rating DESC LIMIT 10;
+    JOIN ratings ON ratings.movie_id = movies.id WHERE year = 2010 ORDER BY ratings.rating DESC
+    AND ORDER BY movies.title ASC LIMIT 10;
