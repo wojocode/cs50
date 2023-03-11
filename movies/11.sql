e3259d320c4d7f37d7f755c31def5c8ed0fc12c3
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-11 15:33:49 +0000

    Sat, Mar 11, 2023, 4:33 PM +01:00

--- movies/11.sql
+++ movies/11.sql
@@ -4,10 +4,6 @@
 
 
 
-
-
-
-
 SELECT movie_id FROM stars WHERE movie_id IN (SELECT id FROM movies WHERE id IN
 (SELECT movie_id FROM stars
 WHERE person_id IN (SELECT id FROM people WHERE name = "Chadwick Boseman")));
