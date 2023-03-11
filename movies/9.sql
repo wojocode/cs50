7b8f9384b143f1895c0f951698189b1e368f3ede
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-11 10:56:35 +0000

    Sat, Mar 11, 2023, 11:56 AM +01:00

--- movies/9.sql
+++ movies/9.sql
@@ -3,4 +3,4 @@ SELECT name FROM people WHERE name IN (SELECT name FROM people WHERE id IN (SELE
 
 SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT id FROM movies WHERE year = 2006)) ORDER BY birth;
 
-SELECT name FROM people 
+SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN )
