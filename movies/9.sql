ece28ff9319d1961041b5e6388b7c055c4c8d79b
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-11 11:00:36 +0000

    Sat, Mar 11, 2023, 12:00 PM +01:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,6 +1,5 @@
-SELECT name FROM people WHERE name IN (SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT id FROM movies WHERE year = 2006)) ORDER BY birth);
+--SELECT COUNT(name) FROM people WHERE name IN (SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT id FROM movies WHERE year = 2004)) ORDER BY birth);
 
 
-SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT id FROM movies WHERE year = 2006)) ORDER BY birth;
+SELECT COUNT(name) FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT id FROM movies WHERE year = 2004)) ORDER BY birth;
 
-SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN )
