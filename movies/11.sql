c412951f3d4b9cd6bfb2de5100b1157871328800
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-13 21:33:40 +0000

    Mon, Mar 13, 2023, 10:33 PM +01:00

--- movies/11.sql
+++ movies/11.sql
@@ -5,4 +5,4 @@ WHERE person_id IN (SELECT id FROM people WHERE name = "Chadwick Boseman")) ORDE
 
 
 
-SELECT rating FROM ratings WHERE movie_id =10514222;
+SELECT rating FROM ratings WHERE movie_id =5301662;
