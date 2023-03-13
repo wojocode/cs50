aeb53e52e53dcb68d15890d2b6c63a622fa76d4a
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-13 10:28:35 +0000

    Mon, Mar 13, 2023, 11:28 AM +01:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,9 +1,9 @@
 SELECT movies.title
 FROM movies,stars,people
-WHERE movies.id = (SELECT movie_id FROM movies WHERE people.name =  "Helena Bonham Carter")
+WHERE movies.id = (SELECT movie_id FROM stars WHERE people.name =  "Helena Bonham Carter")
 AND movies.id = stars.movie_id
-AND people.id = stars.person_id
-AND  title LIKE "F%";
+AND people.id = stars.person_id;
+
 
 
 
