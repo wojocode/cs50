342cd67220ba067374e2e361a2a85ad460d03462
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-13 22:06:04 +0000

    Mon, Mar 13, 2023, 11:06 PM +01:00

--- movies/13.sql
+++ movies/13.sql
@@ -4,5 +4,6 @@
 
 
 -- movie id w których występuje Kevin Bacon
-SELECT COUNT(movie_id) FROM stars
-WHERE person_id IN (SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958);
+SELECT person_id FROM stars WHERE movie_id IN
+(SELECT movie_id FROM stars
+WHERE person_id IN (SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958));
