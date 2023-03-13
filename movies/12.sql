c51ed62b776ad0b0a2ff0d2cbfdceaa3469612df
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-13 11:01:43 +0000

    Mon, Mar 13, 2023, 12:01 PM +01:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,7 +1,6 @@
 SELECT movies.title
 FROM movies,stars,people
 WHERE movies.id IN
-(SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Helena Bonham Carter" OR name = "Johnny Depp"));
 
 
 
@@ -15,11 +14,11 @@ WHERE movies.id IN
 
 
 
-'''
+
+
 SELECT movies.title
 FROM movies,stars,people
 WHERE movies.id IN
 (SELECT movie_id FROM stars WHERE movie_id IN
 ((SELECT stars.movie_id WHERE stars.person_id IN (SELECT id FROM people WHERE name = "Helena Bonham Carter"))
-AND (SELECT stars.movie_id WHERE stars.person_id IN (SELECT id FROM people WHERE name = "Johnny Depp"))));
-'''
+AND (SELECT stars.movie_id WHERE stars.person_id IN (SELECT id FROM people WHERE name = "Johnny Depp")))) GROUP BY movies_id ;
