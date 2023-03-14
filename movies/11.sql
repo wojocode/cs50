a37c8f9f6813332b049b4d69e5b4133a92dbc28f
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-14 15:49:17 +0000

    Tue, Mar 14, 2023, 4:49 PM +01:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,8 +1,8 @@
 
 
-SELECT movie_id  FROM stars JOIN movies ON id =
-(SELECT movie_id FROM ratings
+SELECT movie_id FROM stars WHERE movie_id IN (
+SELECT movie_id FROM ratings
 WHERE movie_id IN (SELECT movie_id FROM stars
-WHERE person_id IN (SELECT id FROM people WHERE name = "Chadwick Boseman")) ORDER BY rating DESC LIMIT 5)
+WHERE person_id IN (SELECT id FROM people WHERE name = "Chadwick Boseman")) ORDER BY rating DESC LIMIT 5);
 ;
 
