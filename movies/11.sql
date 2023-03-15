4a27d66fdc4a9fe170fe9f58f21000624934e62f
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-15 20:27:16 +0000

    Wed, Mar 15, 2023, 9:27 PM +01:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,9 +1,9 @@
 SELECT title FROM movies, stars, people , ratings
 WHERE people.name = "Chadwick Boseman"
 AND people.id = stars.person_id
-AND stars.person_id = movies.id
-AND stars.person_id = people.id
-AND ratings.movie_id = movies.id;
+AND 
+
+
 ORDER BY rating DESC LIMIT 5;
 
 
