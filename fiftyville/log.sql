d33bfc2f0914c7478b029a66750a609bcca4d53d
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-16 22:45:37 +0000

    Thu, Mar 16, 2023, 11:45 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,10 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+--FACTS
+--THE CRIME TOOK PLACE 28 JULY
+-- TOOK PLACE ON HUMPREY STREET
+
+--FIND OUT :
+-- WHO THE THIEF IS
+-- WHERE THE THIEF ESCAPED TO
+-- 
