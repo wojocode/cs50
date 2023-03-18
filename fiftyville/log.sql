a3909e08b358082e2c6aabf6631c724f20a1cc73
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-18 14:47:24 +0000

    Sat, Mar 18, 2023, 3:47 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -32,7 +32,7 @@ SELECT * FROM crime_scene_reports WHERE day = 28 AND month = 7 AND year = 2021 A
 
 -- bakery
 SELECT * FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND HOUR = 10;
--- posible bakery entrance 9 15 am  license plate 5P2BI95
+
 
 
 
