7f9ba660d5107c8b0d241039ca494628507e08c1
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-18 14:57:14 +0000

    Sat, Mar 18, 2023, 3:57 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -34,6 +34,20 @@ SELECT * FROM crime_scene_reports WHERE day = 28 AND month = 7 AND year = 2021 A
 
 SELECT * FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND HOUR = 10 AND minute BETWEEN 14 AND 25;
 
+-- POSSIBLE PEOPLE DETAILS BASED ON LICENSE PLATE
+SELECT * FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND HOUR = 10 AND minute BETWEEN 14 AND 25);
+
+|   id   |  name   |  phone_number  | passport_number | license_plate |
++--------+---------+----------------+-----------------+---------------+
+| 221103 | Vanessa | (725) 555-4692 | 2963008352      | 5P2BI95       |
+| 243696 | Barry   | (301) 555-4174 | 7526138472      | 6P58WS2       |
+| 396669 | Iman    | (829) 555-5269 | 7049073643      | L93JTIZ       |
+| 398010 | Sofia   | (130) 555-0289 | 1695452385      | G412CB7       |
+| 467400 | Luca    | (389) 555-5198 | 8496433585      | 4328GD8       |
+| 514354 | Diana   | (770) 555-1861 | 3592750733      | 322W7JE       |
+| 560886 | Kelsey  | (499) 555-9472 | 8294398571      | 0NTHK55       |
+| 686048 | Bruce   | (367) 555-5533 | 5773159633      | 94KL13X       |
+| 745650 | Sophia  | (027) 555-1068 | 3642612721      | 13FNH73  
 
 
 
