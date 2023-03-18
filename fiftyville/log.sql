959e882fd68e9b121a461134bd7efa7a46649fc5
#Wojtek <111751570+wojocode@users.noreply.github.com>
2023-03-18 12:24:37 +0000

    Sat, Mar 18, 2023, 1:24 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -18,10 +18,24 @@ SELECT * FROM crime_scene_reports WHERE day = 28 AND month = 7 AND year = 2021 A
  Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery.
  Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery.
  ID 295
+-- withnesses description:
+ 1.Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away.
+ If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.
+
+ 2. I don't know the thief's name, but it was someone I recognized.
+ Earlier this morning, before I arrived at Emmas bakery I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.
+
+ 3. As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call,
+ I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow.
+ The thief then asked the person on the other end of the phone to purchase the flight ticket.
+
+
 -- bakery
 SELECT * FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND HOUR = 10 AND minute = 15;
--- NO INFO
 -- posible bakery entrance 9 15 am  license plate 5P2BI95
+
+
+
 
 -- CRIME FACTS
  took place at 10.15am at bakery
